8373fe7ea740ebe429ef40f76e0762ea840b29b2
Julian Oster <github@jlnostr.de>
2016-04-24 12:10:02 +0200

    Make WallabagItem and WallabagTag public.

--- wallabag.Api/Models/WallabagItem.cs
+++ wallabag.Api/Models/WallabagItem.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace wallabag.Api.Models
 {
-    class WallabagItem
+    public class WallabagItem
     {
         [JsonProperty("id")]
         public int Id { get; set; }
--- wallabag.Api/Models/WallabagTag.cs
+++ wallabag.Api/Models/WallabagTag.cs
@@ -2,7 +2,7 @@
 
 namespace wallabag.Api.Models
 {
-    class WallabagTag
+    public class WallabagTag
     {
         [JsonProperty("id")]
         public int Id { get; set; }
