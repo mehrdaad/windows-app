30f136b54aabf2a7bfa5fcf30f5c2e561fb8a8ea
Julian Oster <github@jlnostr.de>
2016-04-25 15:25:22 +0200

    Add static class Credentials which will hold the credentials.

---
+++ wallabag.Api.Tests/Credentials.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallabag.Api.Tests
+{
+    public static class Credentials
+    {
+        // Enter your own credentials here.
+
+        public const string wallabagUrl = "";
+        public const string username = "";
+        public const string password = "";
+        public const string clientId = "";
+        public const string clientSecret = "";
+    }
+}
