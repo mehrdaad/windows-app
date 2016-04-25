8f3020c378fabd051919a8b310d53794904f12c8
Julian Oster <github@jlnostr.de>
2016-04-25 10:48:51 +0200

    Implement AddAsync.

--- wallabag.Api/Components/Add.cs
+++ wallabag.Api/Components/Add.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using wallabag.Api.Models;
 
@@ -6,9 +8,21 @@ namespace wallabag.Api
 {
     public partial class WallabagClient
     {
-        public Task<WallabagItem> AddAsync(Uri uri, string[] tags = null, string title = null)
+        public async Task<WallabagItem> AddAsync(Uri uri, string[] tags = null, string title = null)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(uri.ToString()))
+                throw new ArgumentNullException(nameof(uri));
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("url", uri);
+
+            if (tags != null)
+                parameters.Add("tags", tags.ToCommaSeparatedString());
+            if (title != null)
+                parameters.Add("title", title);
+
+            var jsonString = await ExecuteHttpRequestAsync(HttpRequestMethod.Post, "/entries", parameters);
+            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<WallabagItem>(jsonString));
         }
     }
 }
