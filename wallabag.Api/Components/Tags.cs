b6f64bbff1e7e6e646d9f89142cd6d4b827ebd67
Julian Oster <github@jlnostr.de>
2016-04-25 13:16:04 +0200

    Implement AddTagsAsync and RemoveTagsAsync.

--- wallabag.Api/Components/Tags.cs
+++ wallabag.Api/Components/Tags.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using wallabag.Api.Models;
 
@@ -14,9 +15,37 @@ namespace wallabag.Api
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<IEnumerable<WallabagTag>>(jsonString));
         }
 
-        public Task<bool> AddTagsAsync(int itemId, string[] tags) { throw new NotImplementedException(); }
-        public Task<bool> AddTagsAsync(WallabagItem item, string[] tags) { throw new NotImplementedException(); }
-        public Task<bool> RemoveTagsAsync(int itemId, string[] tags) { throw new NotImplementedException(); }
-        public Task<bool> RemoveTagsAsync(WallabagItem item, string[] tags) { throw new NotImplementedException(); }
+        public async Task<IEnumerable<WallabagTag>> AddTagsAsync(int itemId, string[] tags)
+        {
+            var jsonString = await ExecuteHttpRequestAsync(HttpRequestMethod.Post, $"/entries/{itemId}/tags", new Dictionary<string, object>() { ["tags"] = tags.ToCommaSeparatedString() });
+            var returnedItem = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<WallabagItem>(jsonString));
+            var itemTags = returnedItem.Tags as List<WallabagTag>;
+
+            // Check if the tags are in the returned item
+            foreach (var item in tags)
+                if (itemTags.Where(t => t.Label == item).Count() != 1)
+                    return default(IEnumerable<WallabagTag>);
+
+            return returnedItem.Tags;
+        }
+        public async Task<bool> RemoveTagsAsync(int itemId, WallabagTag[] tags)
+        {
+            var lastJson = string.Empty;
+            foreach (var item in tags)
+                lastJson = await ExecuteHttpRequestAsync(HttpRequestMethod.Delete, $"/entries/{itemId}/tags/{item.Id}");
+
+            var returnedItem = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<WallabagItem>(lastJson));
+            var itemTags = returnedItem.Tags as List<WallabagTag>;
+
+            // Check if the tags aren't in the returned item
+            foreach (var item in tags)
+                if (itemTags.Where(t => t.Label == item.Label).Count() == 1)
+                    return false;
+
+            return true;
+        }
+
+        public Task<IEnumerable<WallabagTag>> AddTagsAsync(WallabagItem item, string[] tags) => AddTagsAsync(item.Id, tags);
+        public Task<bool> RemoveTagsAsync(WallabagItem item, WallabagTag[] tags) => RemoveTagsAsync(item.Id, tags);
     }
 }
