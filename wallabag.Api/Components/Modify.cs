361cefb0927179397c23ffa3c93a6489fdaa3fd8
Julian Oster <github@jlnostr.de>
2016-04-25 11:02:59 +0200

    Implement UnarchiveAsync.

--- wallabag.Api/Components/Modify.cs
+++ wallabag.Api/Components/Modify.cs
@@ -19,8 +19,17 @@ namespace wallabag.Api
             return item.IsRead == true;
         }
         public Task<bool> ArchiveAsync(WallabagItem item) { return ArchiveAsync(item.Id); }
-        public Task<bool> UnarchiveAsync(int itemId) { throw new NotImplementedException(); }
-        public Task<bool> UnarchiveAsync(WallabagItem item) { throw new NotImplementedException(); }
+        public async Task<bool> UnarchiveAsync(int itemId)
+        {
+            if (itemId == 0)
+                throw new ArgumentNullException(nameof(itemId));
+
+            var jsonString = await ExecuteHttpRequestAsync(HttpRequestMethod.Patch, $"/entries/{itemId}", new Dictionary<string, object>() { ["archive"] = false.ToInt() });
+            var item = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<WallabagItem>(jsonString));
+
+            return item.IsRead == false;
+        }
+        public Task<bool> UnarchiveAsync(WallabagItem item) { return UnarchiveAsync(item.Id); }
         public Task<bool> FavoriteAsync(int itemId) { throw new NotImplementedException(); }
         public Task<bool> FavoriteAsync(WallabagItem item) { throw new NotImplementedException(); }
         public Task<bool> UnfavoriteAsync(int itemId) { throw new NotImplementedException(); }
