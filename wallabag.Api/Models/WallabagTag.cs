2439a0848d292c51a9c76399b206b70ee559c19f
Julian Oster <github@jlnostr.de>
2016-04-26 08:39:10 +0200

    Override GetHashCode and Equals for WallabagTag.

--- wallabag.Api/Models/WallabagTag.cs
+++ wallabag.Api/Models/WallabagTag.cs
@@ -11,5 +11,7 @@ namespace wallabag.Api.Models
         public string Label { get; set; }
 
         public override string ToString() => this.Label;
+        public override int GetHashCode() => this.Id;
+        public override bool Equals(object obj) => (obj as WallabagTag).Id.Equals(this.Id);
     }
 }
