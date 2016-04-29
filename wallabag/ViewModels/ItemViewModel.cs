a56ba80c966b4d913c6e1a12ec446e585af7f80f
Julian Oster <github@jlnostr.de>
2016-04-29 19:52:05 +0200

    Add first versions of MainViewModel and ItemViewModel.

---
+++ wallabag/ViewModels/ItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Template10.Mvvm;
+using wallabag.Api.Models;
+
+namespace wallabag.ViewModels
+{
+    public class ItemViewModel : ViewModelBase
+    {
+        public WallabagItem Model { get; private set; }
+
+        public ItemViewModel(WallabagItem Model)
+        {
+            this.Model = Model;
+        }
+    }
+}
---
+++ wallabag/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Template10.Mvvm;
+
+namespace wallabag.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        public ICollection<ItemViewModel> Items { get; set; }
+        
+        public DelegateCommand SyncCommand { get; private set; }
+        public DelegateCommand AddCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
