580011b2748b6917f24223ad358e66334ffcdf7a
Julian Oster <github@jlnostr.de>
2016-05-03 13:21:17 +0200

    Add ImplementPropertyChanged to MainViewModel and make it public.

--- wallabag/ViewModels/MainViewModel.cs
+++ wallabag/ViewModels/MainViewModel.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using PropertyChanged;
+using System.Collections.Generic;
 using Template10.Mvvm;
 
 namespace wallabag.ViewModels
 {
-    class MainViewModel : ViewModelBase
+    [ImplementPropertyChanged]
+    public class MainViewModel : ViewModelBase
     {
         public ICollection<ItemViewModel> Items { get; set; }
-        
+
         public DelegateCommand SyncCommand { get; private set; }
         public DelegateCommand AddCommand { get; private set; }
 
         public MainViewModel()
         {
-            
         }
     }
 }
