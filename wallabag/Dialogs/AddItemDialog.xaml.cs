24b22ee1940c209476c20a025ef3609cc934fab9
Julian Oster <github@jlnostr.de>
2016-05-03 13:10:01 +0200

    Add first draft of AddItemDialog and it's correspodening ViewModel.

---
+++ wallabag/Dialogs/AddItemDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using wallabag.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace wallabag.Dialogs
+{
+    public sealed partial class AddItemDialog : ContentDialog
+    {
+        public AddItemViewModel ViewModel { get { return this.DataContext as AddItemViewModel; } }
+
+        public AddItemDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ wallabag/ViewModels/AddItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace wallabag.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class AddItemViewModel : ViewModelBase
+    {
+        public string UriString { get; set; } = string.Empty;
+        public IEnumerable<string> Tags { get; set; } = new List<string>();
+        public string Title { get; set; } = string.Empty;
+
+        public DelegateCommand AddCommand { get; private set; }
+        public DelegateCommand CancelCommand { get; private set; }
+
+        public AddItemViewModel()
+        {
+            AddCommand = new DelegateCommand(async () => await AddAsync());
+            CancelCommand = new DelegateCommand(() => { if (NavigationService.CanGoBack) NavigationService.GoBack(); });
+        }
+
+        private async Task<bool> AddAsync()
+        {
+            var item = await App.Client.AddAsync(new Uri(UriString), Tags.ToArray(), Title);
+            return item != null;
+        }
+    }
+}
