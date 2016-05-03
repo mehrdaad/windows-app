6c6d86843e5dc31f296808e036eb41502710e9b7
Julian Oster <github@jlnostr.de>
2016-05-03 13:22:41 +0200

    Implement AddCommand and set the default value for Items.

--- wallabag/ViewModels/MainViewModel.cs
+++ wallabag/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Template10.Mvvm;
 
 namespace wallabag.ViewModels
@@ -7,13 +9,14 @@ namespace wallabag.ViewModels
     [ImplementPropertyChanged]
     public class MainViewModel : ViewModelBase
     {
-        public ICollection<ItemViewModel> Items { get; set; }
+        public ICollection<ItemViewModel> Items { get; set; } = new ObservableCollection<ItemViewModel>();
 
         public DelegateCommand SyncCommand { get; private set; }
         public DelegateCommand AddCommand { get; private set; }
 
         public MainViewModel()
         {
+            AddCommand = new DelegateCommand(async () => await new Dialogs.AddItemDialog().ShowAsync());
         }
     }
 }
