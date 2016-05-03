e45415d077e52cd086a6e7a16badfd9dcdf7d67a
Julian Oster <github@jlnostr.de>
2016-05-03 15:12:49 +0200

    Add a static instance of SQLiteAsyncConnection to App.xaml.cs.

--- wallabag/App.xaml.cs
+++ wallabag/App.xaml.cs
@@ -2,25 +2,27 @@
 using System.Threading.Tasks;
 using Template10.Common;
 using Windows.ApplicationModel.Activation;
+using wallabag.Api.Models;
 
 namespace wallabag
 {
     public sealed partial class App : BootStrapper
     {
         public static Api.WallabagClient Client { get; private set; }
+        public static SQLite.SQLiteAsyncConnection Database { get; private set; }
 
-        public App()
-        {
-            this.InitializeComponent();
-        }
-        
-        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        public App() { InitializeComponent(); }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
             if (startKind == StartKind.Launch)
             {
+                Database = new SQLite.SQLiteAsyncConnection("wallabag.db", true);
+                await Database.CreateTableAsync<WallabagItem>();
+                await Database.CreateTableAsync<WallabagTag>();
+
                 NavigationService.Navigate(typeof(Views.MainPage));
             }
-            return Task.CompletedTask;
         }
     }
 }
