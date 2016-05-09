31d82ccd18affca7362204669f2842ed69c8d004
Julian Oster <github@jlnostr.de>
2016-05-09 22:31:10 +0200

    Change used SQLite library and update the implementation.

--- wallabag/App.xaml.cs
+++ wallabag/App.xaml.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+using System;
 using System.Threading.Tasks;
 using Template10.Common;
-using Windows.ApplicationModel.Activation;
 using wallabag.Api.Models;
+using Windows.ApplicationModel.Activation;
 
 namespace wallabag
 {
     public sealed partial class App : BootStrapper
     {
         public static Api.WallabagClient Client { get; private set; }
-        public static SQLite.SQLiteAsyncConnection Database { get; private set; }
+        public static SQLiteConnection Database { get; private set; }
 
         public App() { InitializeComponent(); }
 
@@ -19,11 +22,32 @@ namespace wallabag
             {
                 Client = new Api.WallabagClient(new Uri("https://wallabag.jlnostr.de"), "1_4xy4khl22uck0o8gcs4kw4cwg80s88os0kw0k4so4ssg804ogk", "5wakw2t7uxkwsww480swooow8gsgko8w40wso0w8c4gocsc4ws");
 
-                Database = new SQLite.SQLiteAsyncConnection("wallabag.db", true);
-                await Database.CreateTableAsync<WallabagItem>();
-                await Database.CreateTableAsync<WallabagTag>();
+                var path = (await Windows.Storage.ApplicationData.Current.LocalCacheFolder.CreateFileAsync("wallabag.db", Windows.Storage.CreationCollisionOption.ReplaceExisting)).Path;
+
+                await Task.Factory.StartNew(() =>
+                {
+                    Database = new SQLiteConnection(new SQLitePlatformWinRT(), path, serializer: new JsonSerializer());
+                    Database.CreateTable<WallabagItem>();
+                    Database.CreateTable<WallabagTag>();
+                });
 
                 NavigationService.Navigate(typeof(Views.MainPage));
+            }
+        }
+
+        public class JsonSerializer : IBlobSerializer
+        {
+            public bool CanDeserialize(Type type) => true;
+
+            public object Deserialize(byte[] data, Type type)
+            {
+                var str = System.Text.Encoding.UTF8.GetString(data);
+                return JsonConvert.DeserializeObject(str);
+            }
+
+            public byte[] Serialize<T>(T obj)
+            {
+                return System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj));
             }
         }
     }
