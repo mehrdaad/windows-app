0cc86c4743f06e1162dfe5d094bf2545fb778751
Julian Oster <github@jlnostr.de>
2016-05-14 22:16:52 +0200

    Add first version of ItemPage.
    ViewModel already included.

---
+++ wallabag/ViewModels/ItemPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PropertyChanged;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
+
+namespace wallabag.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class ItemPageViewModel : ViewModelBase
+    {
+        public ItemViewModel Item { get; set; }
+
+        public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        {
+            Item = parameter as ItemViewModel;
+            return base.OnNavigatedToAsync(parameter, mode, state);
+        }
+    }
+}
