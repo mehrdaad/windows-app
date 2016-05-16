c0d7665f179b0a2212f7617a627e72f7cba975a4
Julian Oster <github@jlnostr.de>
2016-05-16 20:49:36 +0200

    Add AccessToken and RefreshToken to SettingsService.
    Also group the settings.

--- wallabag/Services/SettingsService.cs
+++ wallabag/Services/SettingsService.cs
@@ -5,28 +5,44 @@ namespace wallabag.Services
 {
     public class SettingsService
     {
+        #region Singleton & Initialization
+
         public static readonly SettingsService Instance;
         static SettingsService() { Instance = Instance ?? new SettingsService(); }
 
         SettingsHelper _helper;
         private SettingsService() { _helper = new SettingsHelper(); }
 
+        #endregion
+
+        #region Authentication
+
         public Uri WallabagUrl
         {
             get { return _helper.Read(nameof(WallabagUrl), default(Uri)); }
             set { _helper.Write(nameof(WallabagUrl), value); }
         }
-
         public string ClientId
         {
             get { return _helper.Read(nameof(ClientId), string.Empty); }
             set { _helper.Write(nameof(ClientId), value); }
         }
-
         public string ClientSecret
         {
             get { return _helper.Read(nameof(ClientSecret), string.Empty); }
             set { _helper.Write(nameof(ClientSecret), value); }
         }
+        public string AccessToken
+        {
+            get { return _helper.Read(nameof(AccessToken), string.Empty); }
+            set { _helper.Write(nameof(AccessToken), value); }
+        }
+        public string RefreshToken
+        {
+            get { return _helper.Read(nameof(RefreshToken), string.Empty); }
+            set { _helper.Write(nameof(RefreshToken), value); }
+        }
+
+        #endregion
     }
 }
