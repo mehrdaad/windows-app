026326388e02ecaf61fd3d384761407999bf309d
Julian Oster <github@jlnostr.de>
2016-05-16 13:13:10 +0200

    Add a DialogService for opening/closing dialogs.

---
+++ wallabag/Services/DialogService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace wallabag.Services
+{
+    public class DialogService
+    {
+        private static ContentDialog _dialog;
+
+        public static async Task ShowAsync(Dialog dialog)
+        {
+            switch (dialog)
+            {
+                case Dialog.AddItem:
+                    _dialog = new Dialogs.AddItemDialog();
+                    break;
+                case Dialog.EditTags:
+                    break;
+            }
+            await _dialog?.ShowAsync();
+        }
+        public static void HideCurrentDialog() => _dialog?.Hide();
+
+        public enum Dialog
+        {
+            AddItem,
+            EditTags
+        }
+    }
+}
