dfa3c8acb64568ab48902f014388ca79427fd700
Julian Oster <github@jlnostr.de>
2016-05-18 23:02:06 +0200

    Add a BooleanToVisibilityConverter.
    By accepting any value as parameter it can be inverted.

---
+++ wallabag/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace wallabag.Converters
+{
+    class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (parameter == null)
+                return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            else
+                return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return ((Visibility)value == Visibility.Visible) ? true : false;
+        }
+    }
+}
