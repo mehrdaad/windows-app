9fcb2c42fa5e5d7825911dc48846597a46ec0bed
Julian Oster <github@jlnostr.de>
2016-05-18 23:38:05 +0200

    Add dialog for editing tags.

---
+++ wallabag/Dialogs/EditTagsDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using wallabag.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace wallabag.Dialogs
+{
+    public sealed partial class EditTagsDialog : ContentDialog
+    {
+        public EditTagsViewModel ViewModel { get { return this.DataContext as EditTagsViewModel; } }
+
+        public EditTagsDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ wallabag/ViewModels/EditTagsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using PropertyChanged;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using wallabag.Api.Models;
+
+namespace wallabag.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class EditTagsViewModel : ViewModelBase
+    {
+        public IList<WallabagItem> Items { get; set; } = new List<WallabagItem>();
+        public IEnumerable<WallabagTag> Tags { get; set; } = new ObservableCollection<WallabagTag>();
+
+        public DelegateCommand FinishCommand { get; private set; }
+        public DelegateCommand CancelCommand { get; private set; }
+
+        public EditTagsViewModel()
+        {
+            FinishCommand = new DelegateCommand(async () => await FinishAsync());
+            CancelCommand = new DelegateCommand(() => Services.DialogService.HideCurrentDialog());
+        }
+
+        private async Task FinishAsync()
+        {
+            foreach (var item in Items)
+            {
+                var results = await App.Client.AddTagsAsync(item.Id, string.Join(",", Tags).Split(","[0]));
+                (item.Tags as List<WallabagTag>).AddRange(results);
+            }
+        }
+    }
+}
