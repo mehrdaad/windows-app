e1bb12cb5d84b437bd18cf0853999691fb68059f
Julian Oster <github@jlnostr.de>
2016-05-19 10:42:49 +0200

    Update EditTagsViewModel for a better support of single item tags editing.

--- wallabag/ViewModels/EditTagsViewModel.cs
+++ wallabag/ViewModels/EditTagsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using PropertyChanged;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Template10.Mvvm;
 using wallabag.Api.Models;
@@ -10,6 +11,7 @@ namespace wallabag.ViewModels
     [ImplementPropertyChanged]
     public class EditTagsViewModel : ViewModelBase
     {
+        private IEnumerable<WallabagTag> _previousTags;
         public IList<WallabagItem> Items { get; set; } = new List<WallabagItem>();
         public IEnumerable<WallabagTag> Tags { get; set; } = new ObservableCollection<WallabagTag>();
 
@@ -27,15 +29,27 @@ namespace wallabag.ViewModels
             CancelCommand = new DelegateCommand(() => Services.DialogService.HideCurrentDialog());
 
             Items.Add(Item);
+            _previousTags = Item.Tags;
             Tags = new ObservableCollection<WallabagTag>(Item.Tags);
         }
 
         private async Task FinishAsync()
         {
-            foreach (var item in Items)
+            if (_previousTags == null)
             {
-                var results = await App.Client.AddTagsAsync(item.Id, string.Join(",", Tags).Split(","[0]));
-                (item.Tags as List<WallabagTag>).AddRange(results);
+                foreach (var item in Items)
+                {
+                    var results = await App.Client.AddTagsAsync(item.Id, string.Join(",", Tags).Split(","[0]));
+                    (item.Tags as List<WallabagTag>).AddRange(results);
+                }
+            }
+            else
+            {
+                var newTags = Tags.Except(_previousTags);
+                var deletedTags = _previousTags.Except(Tags);
+
+                await App.Client.AddTagsAsync(Items.First().Id, string.Join(",", newTags).Split(","[0]));
+                await App.Client.RemoveTagsAsync(Items.First().Id, deletedTags.ToArray());
             }
         }
     }
