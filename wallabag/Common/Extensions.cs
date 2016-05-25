a43203888b85466580b35b6f70c4c832df5fafa9
Julian Oster <github@jlnostr.de>
2016-05-25 13:15:57 +0200

    Add string.FormatWith extension.

--- wallabag/Common/Extensions.cs
+++ wallabag/Common/Extensions.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace wallabag.Common
 {
@@ -29,6 +28,33 @@ namespace wallabag.Common
             oldList.Clear();
             foreach (var item in newList)
                 oldList.Add(item);
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string FormatWith(this string format, object source)
+        {
+            if (format == null)
+                throw new ArgumentNullException(nameof(format));
+
+            Regex r = new Regex(@"(?<start>\{)+(?<property>[\w\.\[\]]+)(?<format>:[^}]+)?(?<end>\})+",
+                    RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+            string rewrittenFormat = r.Replace(format, delegate (Match m)
+            {
+                Group startGroup = m.Groups["start"];
+                Group propertyGroup = m.Groups["property"];
+                Group formatGroup = m.Groups["format"];
+                Group endGroup = m.Groups["end"];
+
+                var value = (propertyGroup.Value == null)
+                           ? source
+                           : source.GetType().GetRuntimeProperty(propertyGroup.Value).GetValue(source);
+
+                return value.ToString();
+            });
+            return rewrittenFormat;
         }
     }
 }
