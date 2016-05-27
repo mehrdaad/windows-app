d5167c3d5d7a89bfcbd7d52e96ddf50e6caa1dbf
Julian Oster <github@jlnostr.de>
2016-05-27 17:25:24 +0200

    Add WebViewExtensions for an attached Html property.

--- wallabag/Common/Extensions.cs
+++ wallabag/Common/Extensions.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace wallabag.Common
 {
@@ -55,6 +57,23 @@ namespace wallabag.Common
                 return value.ToString();
             });
             return rewrittenFormat;
+        }
+    }
+
+    public static class WebViewExtensions
+    {
+        // Using a DependencyProperty as the backing store for Html.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HtmlProperty =
+            DependencyProperty.RegisterAttached("Html", typeof(string), typeof(WebViewExtensions), new PropertyMetadata(string.Empty, new PropertyChangedCallback(OnHtmlChanged)));
+        
+        public static string GetHtml(DependencyObject obj) => (string)obj.GetValue(HtmlProperty);
+        public static void SetHtml(DependencyObject obj, string value) => obj.SetValue(HtmlProperty, value);
+
+        private static void OnHtmlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WebView wv = d as WebView;
+            if (e.NewValue != null)
+                wv?.NavigateToString((string)e.NewValue);
         }
     }
 }
