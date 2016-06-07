c28ebdce23f811e9c6a1d3e4eddae24c6bc6d85a
Julian Oster <github@jlnostr.de>
2016-06-07 13:37:10 +0200

    Add LoginPageViewModel.

---
+++ wallabag/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace wallabag.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Url { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+
+        public DelegateCommand TestConfigurationCommand { get; private set; }
+        public DelegateCommand ContinueCommand { get; private set; }
+
+        public LoginPageViewModel()
+        {
+
+        }
+    }
+}
