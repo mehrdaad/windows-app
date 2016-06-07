82f1b2644f7b675c826285e404d59618d4e6d42d
Julian Oster <github@jlnostr.de>
2016-06-07 17:24:20 +0200

    Add method for testing the configuration.
    This time I'm doing it right by implementing CanExecute as well :)

--- wallabag/ViewModels/LoginPageViewModel.cs
+++ wallabag/ViewModels/LoginPageViewModel.cs
@@ -1,24 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using PropertyChanged;
+using System;
 using System.Threading.Tasks;
 using Template10.Mvvm;
 
 namespace wallabag.ViewModels
 {
+    [ImplementPropertyChanged]
     public class LoginPageViewModel : ViewModelBase
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Url { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+
+        private bool _TestIsSuccessful = false;
 
         public DelegateCommand TestConfigurationCommand { get; private set; }
         public DelegateCommand ContinueCommand { get; private set; }
 
         public LoginPageViewModel()
+        {
+            TestConfigurationCommand = new DelegateCommand(async () =>
+            {
+                _TestIsSuccessful = await TestConfigurationAsync();
+                ContinueCommand.RaiseCanExecuteChanged();
+            }, () => TestConfigurationCanExecute());
+            ContinueCommand = new DelegateCommand(async () => await ContinueAsync(), () => _TestIsSuccessful);
+
+            PropertyChanged += (s, e) => TestConfigurationCommand.RaiseCanExecuteChanged();
+        }
+
+        private bool TestConfigurationCanExecute()
+        {
+            if (!string.IsNullOrWhiteSpace(Username) &&
+                !string.IsNullOrWhiteSpace(Password) &&
+                !string.IsNullOrWhiteSpace(Url) &&
+                !string.IsNullOrWhiteSpace(ClientId) &&
+                !string.IsNullOrWhiteSpace(ClientSecret) &&
+                ClientId.Length == 52 &&
+                ClientSecret.Length == 50)
+                return true;
+            else
+                return false;
+        }
+        private Task<bool> TestConfigurationAsync()
+        {
+            App.Client.ClientId = ClientId;
+            App.Client.ClientSecret = ClientSecret;
+            App.Client.InstanceUri = new Uri(Url);
+
+            return App.Client.RequestTokenAsync(Username, Password);
+        }
+        private async Task ContinueAsync()
         {
 
         }
