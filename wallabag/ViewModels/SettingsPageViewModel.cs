10ec557da480a5fb4829cd169b0a7dc27ca04908
Julian Oster <github@jlnostr.de>
2016-06-08 22:53:06 +0200

    Add first draft of SettingsPage.

---
+++ wallabag/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace wallabag.ViewModels
+{
+    [ImplementPropertyChanged]    
+    public class SettingsPageViewModel : ViewModelBase
+    {
+        public bool SyncOnStartup { get; set; }
+        public bool AllowTelemetryData { get; set; }
+        public bool NavigateBackAfterReadingAnArticle { get; set; }
+        public bool SyncReadingProgress { get; set; }
+        public DelegateCommand LogoutCommand { get; private set; }
+        public DelegateCommand DeleteDatabaseCommand { get; private set; }
+    }
+}
