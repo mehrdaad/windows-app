e54ffcd2e89d5af98268921d42ac29f228556f18
Julian Oster <github@jlnostr.de>
2016-06-09 12:42:03 +0200

    Implement all of the required DelegateCommands.

--- wallabag/ViewModels/SettingsPageViewModel.cs
+++ wallabag/ViewModels/SettingsPageViewModel.cs
@@ -1,21 +1,93 @@
 ﻿using PropertyChanged;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using Template10.Mvvm;
+using wallabag.Services;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.System;
+using Windows.UI.Xaml;
 
 namespace wallabag.ViewModels
 {
-    [ImplementPropertyChanged]    
+    [ImplementPropertyChanged]
     public class SettingsPageViewModel : ViewModelBase
     {
+        // TODO: Change after merged into master branch.
+        private Uri _changelogUri = new Uri("https://github.com/wallabag/windows-app/blob/v2/CHANGELOG.md");
+        private Uri _documentationUri = new Uri("http://doc.wallabag.org/");
+        private Uri _twitterAccountUri = new Uri("http://doc.wallabag.org/");
+        private Uri _mailUri = new Uri("mailto:wallabag@jlnostr.de");
+        private Uri _githubIssueUri = new Uri("https://github.com/wallabag/windows-app/issues/new");
+        private Uri _rateAppUri = new Uri("ms-windows-store://review/?ProductId=" + Package.Current.Id.FamilyName);
+
         public bool SyncOnStartup { get; set; }
         public bool AllowTelemetryData { get; set; }
         public bool NavigateBackAfterReadingAnArticle { get; set; }
         public bool SyncReadingProgress { get; set; }
+        public string VersionNumber { get; set; }
+
+        public DelegateCommand OpenChangelogCommand { get; private set; }
+        public DelegateCommand OpenDocumentationCommand { get; private set; }
+        public DelegateCommand OpenWallabagTwitterAccountCommand { get; set; }
+        public DelegateCommand ContactDeveloperCommand { get; private set; }
+        public DelegateCommand CreateIssueCommand { get; private set; }
+        public DelegateCommand RateAppCommand { get; private set; }
+        public DelegateCommand TellFriendsCommand { get; private set; }
         public DelegateCommand LogoutCommand { get; private set; }
         public DelegateCommand DeleteDatabaseCommand { get; private set; }
+
+        public SettingsPageViewModel()
+        {
+            VersionNumber = GetVersionNumber();
+
+            OpenChangelogCommand = new DelegateCommand(async () => await Launcher.LaunchUriAsync(_changelogUri));
+            OpenDocumentationCommand = new DelegateCommand(async () => await Launcher.LaunchUriAsync(_documentationUri));
+            OpenWallabagTwitterAccountCommand = new DelegateCommand(async () => await Launcher.LaunchUriAsync(_twitterAccountUri));
+            ContactDeveloperCommand = new DelegateCommand(async () => await Launcher.LaunchUriAsync(_mailUri));
+            CreateIssueCommand = new DelegateCommand(async () => await Launcher.LaunchUriAsync(_githubIssueUri));
+            RateAppCommand = new DelegateCommand(async () => await Launcher.LaunchUriAsync(_rateAppUri));
+            TellFriendsCommand = new DelegateCommand(() => TellFriends());
+            LogoutCommand = new DelegateCommand(() => Logout());
+            DeleteDatabaseCommand = new DelegateCommand(() => DeleteDatabase());
+        }
+
+        private string GetVersionNumber()
+        {
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            return string.Format($"{version.Major}.{version.Minor}.{version.Build}");
+        }
+        private void TellFriends()
+        {
+            // TODO: Update strings.
+            DataTransferManager.GetForCurrentView().DataRequested += (s, e) =>
+            {
+                e.Request.Data.SetText("YO, CHECK OUT WALLABAG!");
+                e.Request.Data.SetWebLink(_changelogUri);
+                e.Request.Data.Properties.ApplicationName = Package.Current.DisplayName;
+                e.Request.Data.Properties.Title = "Have you heard of wallabag?";
+            };
+            DataTransferManager.ShowShareUI();
+        }
+        private void Logout()
+        {
+            SettingsService.Instance.AccessToken = string.Empty;
+            SettingsService.Instance.RefreshToken = string.Empty;
+            SettingsService.Instance.LastTokenRefreshDateTime = DateTime.MinValue;
+
+            Application.Current.Exit();
+        }
+        private void DeleteDatabase()
+        {
+            var path = App.Database.DatabasePath;
+            App.Database.Close();
+
+            File.Delete(path);
+
+            Application.Current.Exit();
+        }
     }
 }
