61d00570a0dea4944ddfbc477db5fa610372da33
Julian Oster <github@jlnostr.de>
2016-06-09 19:09:07 +0200

    Implement OfflineTask.ExecuteAsync().
    Including some changes, due to the limit of IEnumerable.

--- wallabag/Models/OfflineTask.cs
+++ wallabag/Models/OfflineTask.cs
@@ -1,6 +1,9 @@
 ﻿using SQLite.Net.Attributes;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using wallabag.Api.Models;
+using wallabag.Common;
 
 namespace wallabag.Models
 {
@@ -11,21 +14,55 @@ namespace wallabag.Models
 
         public int ItemId { get; set; }
         public OfflineTaskAction Action { get; set; }
-        public IEnumerable<Tag> addTagsList { get; set; }
-        public IEnumerable<Tag> removeTagsList { get; set; }
+        public List<Tag> addTagsList { get; set; }
+        public List<Tag> removeTagsList { get; set; }
 
-        public Task ExecuteAsync()
+        public async Task ExecuteAsync()
         {
-            return Task.CompletedTask;
+            bool executionIsSuccessful = false;
+            switch (Action)
+            {
+                case OfflineTaskAction.MarkAsRead:
+                    executionIsSuccessful = await App.Client.ArchiveAsync(ItemId);
+                    break;
+                case OfflineTaskAction.UnmarkAsRead:
+                    executionIsSuccessful = await App.Client.UnarchiveAsync(ItemId);
+                    break;
+                case OfflineTaskAction.MarkAsStarred:
+                    executionIsSuccessful = await App.Client.FavoriteAsync(ItemId);
+                    break;
+                case OfflineTaskAction.UnmarkAsStarred:
+                    executionIsSuccessful = await App.Client.UnfavoriteAsync(ItemId);
+                    break;
+                case OfflineTaskAction.EditTags:
+                    var newTags = await App.Client.AddTagsAsync(ItemId, string.Join(",", addTagsList).Split(","[0]));
+
+                    var item = App.Database.Get<Item>(i => i.Id == ItemId);
+                    var tags = item.Tags as List<Tag>;
+
+                    if (newTags != null)
+                        tags.Replace(newTags.Convert<WallabagTag, Tag>().ToList());
+
+                    executionIsSuccessful = await App.Client.RemoveTagsAsync(ItemId, removeTagsList.Convert<Tag, WallabagTag>());
+                    break;
+                case OfflineTaskAction.Delete:
+                    executionIsSuccessful = await App.Client.DeleteAsync(ItemId);
+                    break;
+                default:
+                    break;
+            }
+
+            if (executionIsSuccessful)
+                App.Database.Delete(this);
         }
 
         public OfflineTask() { }
-        public OfflineTask(int itemId, OfflineTaskAction action, IEnumerable<Tag> addTagsEnumerable = null, IEnumerable<Tag> removeTagsEnumerable = null)
+        public OfflineTask(int itemId, OfflineTaskAction action, List<Tag> addTagsList = null, List<Tag> removeTagsList = null)
         {
             ItemId = itemId;
             Action = action;
-            addTagsList = addTagsEnumerable;
-            removeTagsList = removeTagsEnumerable;
+            this.addTagsList = addTagsList;
+            this.removeTagsList = removeTagsList;
         }
 
         public enum OfflineTaskAction
