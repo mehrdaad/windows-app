02fd0d51fcbe3ad09550c665dfb0d948a1aa92b2
Julian Oster <github@jlnostr.de>
2016-06-17 19:39:49 +0200

    Add a small Helpers class to wallabag.Common.

---
+++ wallabag/Common/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+
+namespace wallabag.Common
+{
+    class Helpers
+    {
+        public static string LocalizedResource(string resourceName)
+        {
+            return ResourceLoader.GetForCurrentView().GetString(resourceName);
+        }
+    }
+}
