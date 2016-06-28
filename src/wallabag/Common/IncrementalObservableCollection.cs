11c8774b13a7b5ba8f561663b3191ec79b58e479
Julian Oster <github@jlnostr.de>
2016-06-28 18:53:40 +0200

    Add first draft of IncrementalObservableCollection to project.

---
+++ src/wallabag/Common/IncrementalObservableCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+namespace wallabag.Common
+{
+    public class IncrementalObservableCollection<T> : ObservableCollection<T>, ISupportIncrementalLoading
+    {
+        private Func<uint, Task<List<T>>> load;
+        public bool HasMoreItems { get; protected set; }
+
+        public IncrementalObservableCollection(Func<uint, Task<List<T>>> load)
+        {
+            HasMoreItems = true;
+            this.load = load;
+        }
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            return AsyncInfo.Run(async c =>
+            {
+                var data = await load(count);
+
+                foreach (var item in data)
+                    Add(item);
+
+                // TODO: Update HasMoreItems
+
+                return new LoadMoreItemsResult { Count = (uint)data.Count };
+            });
+        }
+    }
+}
