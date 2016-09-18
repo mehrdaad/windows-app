c3219cbd5fd0c08a8567f32e8268efd3f4e9a760
Julian Oster <jlnostr@outlook.de>
2016-09-18 13:45:58 +0200

    Remove deprecated SearchPropertiesViewModel.

--- src/wallabag/Views/MainPage.xaml.cs
+++ src/wallabag/Views/MainPage.xaml.cs
@@ -26,7 +26,6 @@ namespace wallabag.Views
     public sealed partial class MainPage : Page
     {
         public MainViewModel ViewModel { get { return DataContext as MainViewModel; } }
-        public SearchPropertiesViewModel SearchPropertiesViewModel { get { return new SearchPropertiesViewModel(ViewModel.CurrentSearchProperties); } }
 
         public MainPage()
         {
