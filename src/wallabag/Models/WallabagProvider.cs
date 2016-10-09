d82f010f84933f20a207001358d9abeeee54a747
Julian Oster <jlnostr@outlook.de>
2016-10-09 21:21:42 +0200

    Add a WallabagProvider class.
    Before I'm trying to handle a lot of selection events, I'm implementing a list of possible providers. It's also easier to add new providers in the future :)

---
+++ src/wallabag/Models/WallabagProvider.cs
@@ -0,0 +1,20 @@
+﻿using PropertyChanged;
+using System;
+
+namespace wallabag.Models
+{
+    [ImplementPropertyChanged]
+    public class WallabagProvider
+    {
+        public string Name { get; set; }
+        public Uri Url { get; set; }
+        public string ShortDescription { get; set; }
+
+        public WallabagProvider(Uri url, string name, string shortDescription = "")
+        {
+            Url = url;
+            Name = name;
+            ShortDescription = shortDescription;
+        }
+    }
+}
