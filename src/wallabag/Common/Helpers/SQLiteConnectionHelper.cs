355012eed2ddd6d884e2207bed51b96c5e0c8562
Julian Oster <jlnostr@outlook.de>
2016-11-09 10:18:31 +0100

    Update SQLiteConnectionHelper to use transactions the correct way.

--- src/wallabag/Common/Helpers/SQLiteConnectionHelper.cs
+++ src/wallabag/Common/Helpers/SQLiteConnectionHelper.cs
@@ -5,15 +5,28 @@ namespace wallabag.Common.Helpers
 {
     static class SQLiteConnectionHelper
     {
+        private static bool _resetTransaction = false;
+        private static string _transactionPoint = string.Empty;
+
         internal static void RunInTransactionWithUndo(this SQLite.Net.SQLiteConnection conn, Action action)
         {
-            conn.SaveTransactionPoint();
+            _transactionPoint = conn.SaveTransactionPoint();
             action.Invoke();
 
             // Wait two seconds for user feedback, the user can reset the changes during this timespan.
             Task.Delay(TimeSpan.FromSeconds(2)).Wait();
 
-            conn.Close();
+            if (_resetTransaction)
+                conn.RollbackTo(_transactionPoint);
+            else
+                conn.Commit();
+
+            _resetTransaction = false;
+            _transactionPoint = string.Empty;
+        }
+        internal static void UndoChanges()
+        {
+            _resetTransaction = true;
         }
     }
 }
