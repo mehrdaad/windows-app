9539f0d9ef8126559adb87d29b19683ab094e50e
Julian Oster <jlnostr@outlook.de>
2016-11-09 09:55:25 +0100

    Add a SQLiteConnectionHelper to the project.
    I will use the SQLite transactions to undo the changes. Nice feature :)

---
+++ src/wallabag/Common/Helpers/SQLiteConnectionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace wallabag.Common.Helpers
+{
+    static class SQLiteConnectionHelper
+    {
+        internal static void RunInTransactionWithUndo(this SQLite.Net.SQLiteConnection conn, Action action)
+        {
+            conn.SaveTransactionPoint();
+            action.Invoke();
+
+            // Wait two seconds for user feedback, the user can reset the changes during this timespan.
+            Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+
+            conn.Close();
+        }
+    }
+}
