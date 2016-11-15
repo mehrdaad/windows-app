b6ad08ec36d67a1de73c400939694e43a36d7315
Julian Oster <jlnostr@outlook.de>
2016-11-15 18:59:16 +0100

    Add an OfflineTaskService to the project.
    Instead of handling the execution in the model (which is a bad approach btw, invalid with MVVM), a new service class will handle everything related to offline tasks.

--- src/wallabag/Models/OfflineTask.cs
+++ src/wallabag/Models/OfflineTask.cs
@@ -1,13 +1,5 @@
-﻿using GalaSoft.MvvmLight.Messaging;
-using SQLite.Net.Attributes;
-using System;
+﻿using SQLite.Net.Attributes;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
-using wallabag.Api.Models;
-using wallabag.Common.Helpers;
-using wallabag.Common.Messages;
 
 namespace wallabag.Models
 {
@@ -18,116 +10,13 @@ namespace wallabag.Models
 
         public int ItemId { get; set; }
         public OfflineTaskAction Action { get; set; }
-        public List<Tag> addTagsList { get; set; }
-        public List<Tag> removeTagsList { get; set; }
+        public List<Tag> AddedTags { get; set; }
+        public List<Tag> RemovedTags { get; set; }
 
         public string Url { get; set; }
         public List<string> Tags { get; set; }
 
         public OfflineTask() { }
-
-        public async Task ExecuteAsync()
-        {
-            if (GeneralHelper.InternetConnectionIsAvailable == false)
-                return;
-
-            bool executionIsSuccessful = false;
-            switch (Action)
-            {
-                case OfflineTaskAction.MarkAsRead:
-                    executionIsSuccessful = await App.Client.ArchiveAsync(ItemId);
-                    break;
-                case OfflineTaskAction.UnmarkAsRead:
-                    executionIsSuccessful = await App.Client.UnarchiveAsync(ItemId);
-                    break;
-                case OfflineTaskAction.MarkAsStarred:
-                    executionIsSuccessful = await App.Client.FavoriteAsync(ItemId);
-                    break;
-                case OfflineTaskAction.UnmarkAsStarred:
-                    executionIsSuccessful = await App.Client.UnfavoriteAsync(ItemId);
-                    break;
-                case OfflineTaskAction.EditTags:
-                    var item = App.Database.Get<Item>(i => i.Id == ItemId);
-
-                    if (addTagsList?.Count > 0)
-                    {
-                        var newTags = await App.Client.AddTagsAsync(ItemId, addTagsList.ToStringArray());
-
-                        if (newTags != null)
-                        {
-                            var convertedTags = new ObservableCollection<Tag>();
-                            foreach (var tag in newTags)
-                                convertedTags.Add(tag);
-
-                            item.Tags.Replace(convertedTags);
-                        }
-                    }
-                    if (removeTagsList?.Count > 0)
-                    {
-                        List<WallabagTag> tagsToRemove = new List<WallabagTag>();
-                        foreach (var tag in removeTagsList)
-                            tagsToRemove.Add(tag);
-
-                        if (await App.Client.RemoveTagsAsync(ItemId, tagsToRemove))
-                            foreach (var tag in removeTagsList)
-                                if (item.Tags.Contains(tag))
-                                    item.Tags.Remove(tag);
-                    }
-
-                    executionIsSuccessful = App.Database.Update(item) == 1;
-                    break;
-                case OfflineTaskAction.AddItem:
-                    var newItem = await App.Client.AddAsync(new Uri(Url), Tags);
-
-                    if (newItem != null)
-                    {
-                        App.Database.InsertOrReplace((Item)newItem);
-                        Messenger.Default.Send(new UpdateItemMessage(newItem.Id));
-                    }
-
-                    executionIsSuccessful = newItem != null;
-                    break;
-                case OfflineTaskAction.Delete:
-                    executionIsSuccessful = await App.Client.DeleteAsync(ItemId);
-                    break;
-                default:
-                    break;
-            }
-
-            if (executionIsSuccessful)
-            {
-                App.Database.Delete(this);
-                App.OfflineTaskRemoved?.Invoke(this, this);
-            }
-        }
-        public static void Add(string url, IEnumerable<string> newTags, string title = "", bool invokeAddedEvent = true)
-        {
-            var newTask = new OfflineTask();
-
-            newTask.ItemId = LastItemId;
-            newTask.Action = OfflineTaskAction.AddItem;
-            newTask.Url = url;
-            newTask.Tags = newTags.ToList();
-
-            App.Database.Insert(newTask);
-
-            if (invokeAddedEvent)
-                App.OfflineTaskAdded?.Invoke(null, newTask);
-        }
-        public static void Add(int itemId, OfflineTaskAction action, List<Tag> addTagsList = null, List<Tag> removeTagsList = null)
-        {
-            var newTask = new OfflineTask();
-
-            newTask.ItemId = itemId;
-            newTask.Action = action;
-            newTask.addTagsList = addTagsList;
-            newTask.removeTagsList = removeTagsList;
-
-            App.Database.Insert(newTask);
-            App.OfflineTaskAdded?.Invoke(null, newTask);
-        }
-
-        internal static int LastItemId => App.Database.ExecuteScalar<int>("select Max(ID) from 'Item'", new object[0]);
 
         public enum OfflineTaskAction
         {
---
+++ src/wallabag/Services/OfflineTaskService.cs
@@ -0,0 +1,125 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using wallabag.Api.Models;
+using wallabag.Common.Helpers;
+using wallabag.Common.Messages;
+using wallabag.Models;
+using static wallabag.Models.OfflineTask;
+
+namespace wallabag.Services
+{
+    class OfflineTaskService
+    {
+        private static Dictionary<int, OfflineTask> _tasks = new Dictionary<int, OfflineTask>();
+
+        public static Task ExecuteAsync(OfflineTask task) => ExecuteAsync(task.Id);
+        public static async Task ExecuteAsync(int taskId)
+        {
+            var task = _tasks[taskId];
+
+            if (GeneralHelper.InternetConnectionIsAvailable == false)
+                return;
+
+            bool executionIsSuccessful = false;
+            switch (task.Action)
+            {
+                case OfflineTaskAction.MarkAsRead:
+                    executionIsSuccessful = await App.Client.ArchiveAsync(task.ItemId);
+                    break;
+                case OfflineTaskAction.UnmarkAsRead:
+                    executionIsSuccessful = await App.Client.UnarchiveAsync(task.ItemId);
+                    break;
+                case OfflineTaskAction.MarkAsStarred:
+                    executionIsSuccessful = await App.Client.FavoriteAsync(task.ItemId);
+                    break;
+                case OfflineTaskAction.UnmarkAsStarred:
+                    executionIsSuccessful = await App.Client.UnfavoriteAsync(task.ItemId);
+                    break;
+                case OfflineTaskAction.EditTags:
+                    var item = App.Database.Get<Item>(i => i.Id == task.ItemId);
+
+                    if (task.AddedTags?.Count > 0)
+                    {
+                        var newTags = await App.Client.AddTagsAsync(task.ItemId, task.AddedTags.ToStringArray());
+
+                        if (newTags != null)
+                        {
+                            var convertedTags = new ObservableCollection<Tag>();
+                            foreach (var tag in newTags)
+                                convertedTags.Add(tag);
+
+                            item.Tags.Replace(convertedTags);
+                        }
+                    }
+                    if (task.RemovedTags?.Count > 0)
+                    {
+                        List<WallabagTag> tagsToRemove = new List<WallabagTag>();
+                        foreach (var tag in task.RemovedTags)
+                            tagsToRemove.Add(tag);
+
+                        if (await App.Client.RemoveTagsAsync(task.ItemId, tagsToRemove))
+                            foreach (var tag in task.RemovedTags)
+                                if (item.Tags.Contains(tag))
+                                    item.Tags.Remove(tag);
+                    }
+
+                    executionIsSuccessful = App.Database.Update(item) == 1;
+                    break;
+                case OfflineTaskAction.AddItem:
+                    var newItem = await App.Client.AddAsync(new Uri(task.Url), task.Tags);
+
+                    if (newItem != null)
+                    {
+                        App.Database.InsertOrReplace((Item)newItem);
+                        Messenger.Default.Send(new UpdateItemMessage(newItem.Id));
+                    }
+
+                    executionIsSuccessful = newItem != null;
+                    break;
+                case OfflineTaskAction.Delete:
+                    executionIsSuccessful = await App.Client.DeleteAsync(task.ItemId);
+                    break;
+                default:
+                    break;
+            }
+
+            if (executionIsSuccessful)
+            {
+                App.Database.Delete(task);
+                App.OfflineTaskRemoved?.Invoke(task, task);
+            }
+        }
+        public static void AddTask(string url, IEnumerable<string> newTags = null, string title = "", bool invokeAddedEvent = true)
+        {
+            var newTask = new OfflineTask();
+
+            newTask.ItemId = LastItemId;
+            newTask.Action = OfflineTaskAction.AddItem;
+            newTask.Url = url;
+            newTask.Tags = newTags.ToList();
+
+            App.Database.Insert(newTask);
+
+            if (invokeAddedEvent)
+                App.OfflineTaskAdded?.Invoke(null, newTask);
+        }
+        public static void AddTask(int itemId, OfflineTaskAction action, List<Tag> addTagsList = null, List<Tag> removeTagsList = null)
+        {
+            var newTask = new OfflineTask();
+
+            newTask.ItemId = itemId;
+            newTask.Action = action;
+            newTask.AddedTags = addTagsList;
+            newTask.RemovedTags = removeTagsList;
+
+            App.Database.Insert(newTask);
+            App.OfflineTaskAdded?.Invoke(null, newTask);
+        }
+
+        internal static int LastItemId => App.Database.ExecuteScalar<int>("select Max(ID) from 'Item'", new object[0]);
+    }
+}
