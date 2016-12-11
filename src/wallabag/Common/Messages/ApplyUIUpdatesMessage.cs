eb57d9e6e39542ffc8a0c0381741e5ac3c787bb5
Julian Oster <jlnostr@outlook.de>
2016-12-11 22:10:38 +0100

    Add an ApplyUIUpdatesMessage.

---
+++ src/wallabag/Common/Messages/ApplyUIUpdatesMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace wallabag.Common.Messages
+{
+    class ApplyUIUpdatesMessage
+    {
+        public bool ClearQueue { get; set; } = false;
+
+        public ApplyUIUpdatesMessage() { }
+        public ApplyUIUpdatesMessage(bool clearQueue)
+        {
+            ClearQueue = clearQueue;
+        }
+    }
+}
