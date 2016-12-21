e7ac627ed7ed515cb597e670cebde6a54d095bf9
Julian Oster <jlnostr@outlook.de>
2016-12-21 20:24:29 +0100

    Add PowerShell scripts for HockeyApp upload.
    They won't be used yet, because I don't have the script to upload the app directly to the store. And having a lot of builds visible in HockeyApp that are never used is kind of useless.

---
+++ appveyor/deployment/CreateSymbolsForHockeyApp.ps1
@@ -0,0 +1,36 @@
+﻿Add-Type -Assembly System.IO.Compression.FileSystem
+
+$rootPath = $ENV:APPVEYOR_BUILD_FOLDER;
+$versionNumber = $ENV:APPVEYOR_BUILD_VERSION;
+$platforms = "x86|x64|ARM".Split("|");
+$completeVersionNumber = "$versionNumber.0";
+
+$appSymbolsDirectory = [System.IO.Path]::Combine($rootPath, "src\wallabag\AppPackages\wallabag_$($versionNumber).0_Test\");
+$finalSymbolRootDirectory = [System.IO.Path]::Combine($rootPath, "hockeyapp-symbols");
+
+if ([System.IO.DirectoryInfo]::new($finalSymbolRootDirectory).Exists -eq $false)
+{
+    [System.IO.Directory]::CreateDirectory($finalSymbolRootDirectory);
+
+    foreach ($platform in $platforms)
+    {
+        $resultSymbolDirectory = [System.IO.Directory]::CreateDirectory("$($finalSymbolRootDirectory)\$($platform)");
+        $librariesSymbolDirectory = [System.IO.Path]::Combine($rootPath, "src\wallabag\obj\$($platform)\Release\ilc\in\");
+               
+        Write-Host "Extracting $($platform) symbols for application...";
+
+        $appSymFile = [System.IO.Path]::Combine($appSymbolsDirectory, "wallabag_$($versionNumber).0_$($platform).appxsym");
+  
+        $zipFile = [System.IO.Compression.ZipFile]::ExtractToDirectory($appSymFile, $resultSymbolDirectory.FullName);
+
+        Write-Host "Copying symbols for libraries...";        
+        Copy-Item -Path "$($librariesSymbolDirectory)\*" -Destination "$($resultSymbolDirectory.FullName)" -Filter "*.pdb" -Exclude "wallabag.pdb";
+
+        Write-Host "Creating ZIP file for HockeyApp...";
+        [System.IO.Compression.ZipFile]::CreateFromDirectory($resultSymbolDirectory.FullName, "$($finalSymbolRootDirectory)\symbols-$($platform).zip");
+    }
+}
+else
+{
+    Write-Host "Symbols directory already exists. Aborting." -ForegroundColor Red
+}
---
+++ appveyor/deployment/UploadSymbolsToHockeyApp.ps1
@@ -0,0 +1,34 @@
+$appSymbolsDirectory = [System.IO.Path]::Combine($rootPath, "src\wallabag\AppPackages\wallabag_$($completeVersionNumber)_Test\");
+$finalSymbolRootDirectory = [System.IO.Path]::Combine($rootPath, "hockeyapp-symbols");
+
+$HockeyAppAppID = $ENV:HOCKEYAPP_APP_ID;
+$HockeyAppApiToken = $ENV:HOCKEYAPP_API_TOKEN;
+ 
+Write-Host "Create new HockeyApp version..."
+
+$create_url = "https://rink.hockeyapp.net/api/2/apps/$HockeyAppAppID/app_versions/new"
+$response = Invoke-RestMethod -Method POST -Uri $create_url  -Header @{ "X-HockeyAppToken" = $HockeyAppApiToken } -Body @{bundle_version = $completeVersionNumber}
+$update_url = "https://rink.hockeyapp.net/api/2/apps/$($HockeyAppAppID)/app_versions/$($response.id)"
+
+$symbolFiles = Get-ChildItem $finalSymbolRootDirectory -Filter "*.zip"
+
+Write-Host "Uploading symbols to HockeyApp..."
+foreach ($zipFile in $symbolFiles)
+{
+    Write-Host "Uploading $zipFile to HockeyApp...";
+    $fileBin = [IO.File]::ReadAllBytes($zipFile.FullName)
+    $enc = [System.Text.Encoding]::GetEncoding("ISO-8859-1")
+    $fileEnc = $enc.GetString($fileBin)
+    $boundary = [System.Guid]::NewGuid().ToString()
+    $LF = "`r`n"
+
+    $bodyLines = (
+        "--$boundary",
+	    "content-transfer-encoding: base64",
+	    "Content-Disposition: form-data; content-transfer-encoding: `"base64`"; name=`"dsym`"; filename=`"$([System.IO.Path]::GetFileName($zipFile))`"$LF",$fileEnc,
+        "--$boundary",
+        "Content-Disposition: form-data; name=`"status`"$LF","2",
+        "--$boundary--$LF") -join $LF
+	
+    Invoke-RestMethod -Uri $update_url -Method PUT -Headers @{ "X-HockeyAppToken" = $HockeyAppApiToken } -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $bodyLines
+}
