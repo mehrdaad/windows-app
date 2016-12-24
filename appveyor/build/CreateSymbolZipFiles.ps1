8c89ca8e0b8eb2f1595188e62590dffbdfce5697
Julian Oster <jlnostr@outlook.de>
2016-12-24 22:32:34 +0100

    Update build scripts to push artifacts.
    Setting the artifacts in the appveyor.yml didn't work as expected in my tests, so I'm now pushing the artifacts to AppVeyor using PowerShell.

--- appveyor/build/CreateSymbolZipFiles.ps1
+++ appveyor/build/CreateSymbolZipFiles.ps1
@@ -3,9 +3,8 @@
 $rootPath = $ENV:APPVEYOR_BUILD_FOLDER;
 $versionNumber = $ENV:APPVEYOR_BUILD_VERSION;
 $platforms = "x86|x64|ARM".Split("|");
-$completeVersionNumber = "$versionNumber.0";
 
-$appSymbolsDirectory = [System.IO.Path]::Combine($rootPath, "src\wallabag\AppPackages\wallabag_$($completeVersionNumber)_Test\");
+$appSymbolsDirectory = Resolve-Path ([System.IO.Path]::Combine($rootPath, "src\wallabag\AppPackages\wallabag_*_Test\"));
 $finalSymbolRootDirectory = [System.IO.Path]::Combine($rootPath, "hockeyapp-symbols");
 
 if ([System.IO.DirectoryInfo]::new($finalSymbolRootDirectory).Exists -eq $false)
@@ -14,19 +13,26 @@ if ([System.IO.DirectoryInfo]::new($finalSymbolRootDirectory).Exists -eq $false)
 
     foreach ($platform in $platforms)
     {
-        $resultSymbolDirectory = [System.IO.Directory]::CreateDirectory("$($finalSymbolRootDirectory)\$($platform)");
-        $librariesSymbolDirectory = [System.IO.Path]::Combine($rootPath, "src\wallabag\obj\$($platform)\Release\ilc\in\");
+        $resultSymbolDirectory = [System.IO.Directory]::CreateDirectory("$finalSymbolRootDirectory\$platform");
+        $librariesSymbolDirectory = [System.IO.Path]::Combine($rootPath, "src\wallabag\obj\$platform\Release\ilc\in\");
                
-        Write-Host "Extracting $($platform) symbols for application...";
-		$appSymFile = [System.IO.Path]::Combine($appSymbolsDirectory, "wallabag_$($completeVersionNumber)_$($platform).appxsym");
-        $zipFile = [System.IO.Compression.ZipFile]::ExtractToDirectory($appSymFile, $resultSymbolDirectory.FullName);
+        Write-Host "Extracting $platform symbols for application...";
+		$appSymFile = Get-ChildItem -Path $appSymbolsDirectory -Filter "wallabag_*_$platform.appxsym" | Select-Object -First 1
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($appSymFile.FullName, $resultSymbolDirectory.FullName);
 
         Write-Host "Copying symbols for libraries...";        
-        Copy-Item -Path "$($librariesSymbolDirectory)\*" -Destination "$($resultSymbolDirectory.FullName)" -Filter "*.pdb" -Exclude "wallabag.pdb";
+        Copy-Item -Path "$librariesSymbolDirectory\*" -Destination "$($resultSymbolDirectory.FullName)" -Filter "*.pdb" -Exclude "wallabag.pdb";
 
         Write-Host "Creating ZIP file for HockeyApp...";
-        [System.IO.Compression.ZipFile]::CreateFromDirectory($resultSymbolDirectory.FullName, "$($finalSymbolRootDirectory)\symbols-$($platform).zip");
+        $zipFilePath = "$finalSymbolRootDirectory\symbols-$platform.zip";
+        [System.IO.Compression.ZipFile]::CreateFromDirectory($resultSymbolDirectory.FullName, $zipFilePath);
+        
+        Push-AppveyorArtifact $zipFilePath;
     }
+
+    Write-Host "Pushing appxbundle to AppVeyor artifacts..."
+    $appxbundleFile = Get-ChildItem -Path $appSymbolsDirectory -Filter "wallabag*.appxbundle" | Select-Object -First 1
+    Push-AppveyorArtifact $appxbundleFile.FullName;
 }
 else
 {
