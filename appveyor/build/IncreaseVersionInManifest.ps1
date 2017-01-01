c3722ad0fa4cffd3d5618e5fa744064e6163899b
Julian Oster <jlnostr@outlook.de>
2017-01-01 15:50:33 +0100

    Update IncreaseVersionInManifest.ps1

--- appveyor/build/IncreaseVersionInManifest.ps1
+++ appveyor/build/IncreaseVersionInManifest.ps1
@@ -1,9 +1,9 @@
-﻿$manifestfile = Get-Item -Path "$ENV:APPVEYOR_BUILD_FOLDER\Package.appxmanifest"
+﻿$manifestfile = Get-Item -Path "$ENV:APPVEYOR_BUILD_FOLDER\src\wallabag\Package.appxmanifest"
   
 $manifestXml = New-Object -TypeName System.Xml.XmlDocument
 $manifestXml.Load($manifestfile.Fullname)
 
-$updatedVersion = [Version]$ENV:APPVEYOR_BUILD_VERSION + '.0';
+$updatedVersion = [Version]"$ENV:APPVEYOR_BUILD_VERSION.0";
 
 $manifestXml.Package.Identity.Version = [String]$updatedVersion
 $manifestXml.save($manifestfile.FullName)
