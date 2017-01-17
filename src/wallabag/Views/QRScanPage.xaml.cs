545c4fdc79336a687fa56b41e3d38a425599f5f1
Julian Oster <jlnostr@outlook.de>
2017-01-17 20:46:23 +0100

    Add separate page for QR code scanning.

---
+++ src/wallabag/Views/QRScanPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using wallabag.Common.Helpers;
+using Windows.UI.Xaml.Controls;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace wallabag.Views
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class QRScanPage : Page
+    {
+        public QRScanPage()
+        {
+            InitializeComponent();
+
+            scannerControl.TopText = GeneralHelper.LocalizedResource("HoldCameraOntoQRCodeMessage");
+        }
+    }
+}
