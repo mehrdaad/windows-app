d1a56909780ca7355b25a2586451ef7976a1469b
Julian Oster <jlnostr@outlook.de>
2017-01-20 21:49:40 +0100

    Create an UriHelper.
    In all of my tests I've never checked for URIs where wallabag is installed in a subfolder which is not the most unusual case. The UriHelper is helping in creating valid URIs for this usecase.

---
+++ src/wallabag/Common/Helpers/UriHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace wallabag.Common.Helpers
+{
+    public static class UriHelper
+    {
+        public static Uri Append(this Uri baseUri, string relativeString)
+        {
+            if (baseUri.IsAbsoluteUri == false)
+                throw new FormatException("The URI is not absolute.");
+
+            string finalPath = baseUri.AbsolutePath;
+
+            if (finalPath.EndsWith("/") == false)
+                finalPath += "/";
+
+            if (relativeString.StartsWith("/"))
+                relativeString = relativeString.TrimStart('/');
+
+            finalPath += relativeString;
+
+            var result = new Uri(baseUri, finalPath);
+            return result;
+        }
+
+        public static bool IsValidUri(this string uriString)
+        {
+            try
+            {
+                var x = new Uri(uriString);
+                return true;
+            }
+            catch (UriFormatException) { return false; }
+        }
+    }
+}
+
