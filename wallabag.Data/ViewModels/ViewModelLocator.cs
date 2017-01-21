636c2b79f1ee8def80571b2f0e9265059d96caa7
Julian Oster <jlnostr@outlook.de>
2017-01-21 16:17:53 +0100

    Add a ViewModelLocator.
    IOC and Dependency Injection will be used in more parts of the app for the future, so writing unit tests is going to be easier than ever.

---
+++ wallabag.Data/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace wallabag.Data.ViewModels
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            //Create run time view services and models
+            SimpleIoc.Default.Register<Api.IWallabagClient, Api.WallabagClient>();
+            //SimpleIoc.Default.Register<INavigationService, NavigationService>();
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+    }
+}
