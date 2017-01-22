33b8179a15c633317870615b220497155e0e9d66
Julian Oster <jlnostr@outlook.de>
2017-01-22 14:00:20 +0100

    Add a Pages class.
    It contains strings for all relevant pages following the MVVM pattern. The NavigationService (from MVVM Light) will then resolve the string to the correct page.

---
+++ src/wallabag.Data/Common/Pages.cs
@@ -0,0 +1,11 @@
+﻿namespace wallabag.Data.Common
+{
+    class Pages
+    {
+        public const string MainPage = "MainPage";
+        public const string ItemPage = "ItemPage";
+        public const string SettingsPage = "SettingsPage";
+        public const string QRScanPage = "QRScanPage";
+        public const string LoginPage = "LoginPage";
+    }
+}
