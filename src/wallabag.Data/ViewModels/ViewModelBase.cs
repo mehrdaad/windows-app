dd792b52f52ba6bf57719297ab3cfc398219042f
Julian Oster <jlnostr@outlook.de>
2017-01-22 14:02:13 +0100

    Add common properties to the ViewModelBase.

--- src/wallabag.Data/ViewModels/ViewModelBase.cs
+++ src/wallabag.Data/ViewModels/ViewModelBase.cs
@@ -1,7 +1,12 @@
-﻿namespace wallabag.Data.ViewModels
+﻿using GalaSoft.MvvmLight.Views;
+using wallabag.Api;
+
+namespace wallabag.Data.ViewModels
 {
     public class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase
     {
-
+        internal IWallabagClient Client;
+        internal SQLite.Net.SQLiteConnection Database;
+        internal INavigationService Navigation;
     }
 }
