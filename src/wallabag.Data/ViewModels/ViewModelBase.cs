62092c47e8bf362d6c98d25d1e0fa02753769391
Julian Oster <jlnostr@outlook.de>
2017-01-22 17:42:33 +0100

    Restore ViewModelBase.
    Visual Studio destroyed my file. Sometimes I have the feeling that I shouldn't use beta software in production.

---
+++ src/wallabag.Data/ViewModels/ViewModelBase.cs
@@ -0,0 +1,28 @@
+using GalaSoft.MvvmLight.Views;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using wallabag.Api;
+using wallabag.Data.Common;
+using wallabag.Data.Services;
+
+namespace wallabag.Data.ViewModels
+{
+    public class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase, INavigable
+    {
+        internal IWallabagClient Client;
+        internal static SQLite.Net.SQLiteConnection Database;
+        internal INavigationService Navigation;
+        internal Services.IDialogService DialogService;
+        internal ILoggingService LoggingService;
+
+        public virtual Task OnNavigatedToAsync(object parameter, IDictionary<string, object> state)
+        {
+            return Task.CompletedTask;
+        }
+        public virtual Task OnNavigatedFromAsync(IDictionary<string, object> pageState)
+        {
+            base.Cleanup();
+            return Task.CompletedTask;
+        }
+    }
+}
