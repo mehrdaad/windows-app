423243ed91fa3a1008fcb99139ac82a4884fbad3
Julian Oster <jlnostr@outlook.de>
2017-01-22 16:55:40 +0100

    Add a logging interface.

---
+++ src/wallabag.Data/Services/ILoggingService.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace wallabag.Data.Services
+{
+    public interface ILoggingService
+    {
+        void WriteLine(string text, LoggingCategory category = LoggingCategory.Debug, [CallerMemberName] string member = "", [CallerLineNumber] int lineNumber = 0);
+        void WriteLineIf(bool condition, string text, LoggingCategory category = LoggingCategory.Debug, [CallerMemberName] string member = "", [CallerLineNumber] int lineNumber = 0);
+    }
+
+    public enum LoggingCategory { Info, Warning, Critical, Debug }
+}
