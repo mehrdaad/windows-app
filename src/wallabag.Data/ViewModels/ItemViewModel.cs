cb57905f42f6904fc600dd990852d04ee8380f94
Julian Oster <jlnostr@outlook.de>
2017-01-22 21:39:25 +0100

    Use logging on ItemViewModel.

--- src/wallabag.Data/ViewModels/ItemViewModel.cs
+++ src/wallabag.Data/ViewModels/ItemViewModel.cs
@@ -1,8 +1,9 @@
 ﻿using GalaSoft.MvvmLight.Command;
-using PropertyChanged;
+using GalaSoft.MvvmLight.Ioc;
 using System;
 using System.ComponentModel;
 using System.Windows.Input;
+using wallabag.Data.Common;
 using wallabag.Data.Models;
 using wallabag.Data.Services;
 using Windows.ApplicationModel.DataTransfer;
@@ -10,13 +11,12 @@ using Windows.System;
 
 namespace wallabag.Data.ViewModels
 {
-    [ImplementPropertyChanged]
     public class ItemViewModel : ViewModelBase, IComparable
     {
         public Item Model { get; private set; }
 
-        public string TagsString { get { return string.Join(", ", Model.Tags); } }
-        public bool TagsAreExisting { get { return Model.Tags.Count > 0; } }
+        public string TagsString => string.Join(", ", Model.Tags);
+        public bool TagsAreExisting => Model.Tags.Count > 0;
 
         public ICommand MarkAsReadCommand { get; private set; }
         public ICommand UnmarkAsReadCommand { get; private set; }
@@ -29,46 +29,60 @@ namespace wallabag.Data.ViewModels
 
         public ItemViewModel(Item Model)
         {
+            LoggingService.WriteLine("Creating new instance of ItemViewModel.");
+            LoggingService.WriteLine($"{Model.Id} | {Model.Title} | {Model.Url}");
+
             this.Model = Model;
 
-            (Model as INotifyPropertyChanged).PropertyChanged += (s, e) => { RaisePropertyChanged(nameof(Model)); };
+            (Model as INotifyPropertyChanged).PropertyChanged += (s, e) =>
+            {
+                LoggingService.WriteLine($"Model with ID {Model.Id} was updated.");
+                RaisePropertyChanged(nameof(Model));
+            };
             Model.Tags.CollectionChanged += (s, e) =>
             {
+                LoggingService.WriteLine($"Tags of model with ID {Model.Id} were updated.");
                 RaisePropertyChanged(nameof(TagsString));
                 RaisePropertyChanged(nameof(TagsAreExisting));
             };
 
             MarkAsReadCommand = new RelayCommand(() =>
             {
+                LoggingService.WriteLine($"Marking item {Model.Id} as read.");
                 Model.IsRead = true;
                 UpdateItem();
                 OfflineTaskService.Add(Model.Id, OfflineTask.OfflineTaskAction.MarkAsRead);
             });
             UnmarkAsReadCommand = new RelayCommand(() =>
             {
+                LoggingService.WriteLine($"Marking item {Model.Id} as unread.");
                 Model.IsRead = false;
                 UpdateItem();
                 OfflineTaskService.Add(Model.Id, OfflineTask.OfflineTaskAction.UnmarkAsRead);
             });
             MarkAsStarredCommand = new RelayCommand(() =>
             {
+                LoggingService.WriteLine($"Marking item {Model.Id} as favorite.");
                 Model.IsStarred = true;
                 UpdateItem();
                 OfflineTaskService.Add(Model.Id, OfflineTask.OfflineTaskAction.MarkAsStarred);
             });
             UnmarkAsStarredCommand = new RelayCommand(() =>
             {
+                LoggingService.WriteLine($"Marking item {Model.Id} as unfavorite.");
                 Model.IsStarred = false;
                 UpdateItem();
                 OfflineTaskService.Add(Model.Id, OfflineTask.OfflineTaskAction.UnmarkAsStarred);
             });
             DeleteCommand = new RelayCommand(() =>
             {
+                LoggingService.WriteLine($"Deleting item {Model.Id}.");
                 Database.Delete(Model);
                 OfflineTaskService.Add(Model.Id, OfflineTask.OfflineTaskAction.Delete);
             });
             ShareCommand = new RelayCommand(() =>
             {
+                LoggingService.WriteLine($"Sharing item {Model.Id}.");
                 DataTransferManager.GetForCurrentView().DataRequested += (s, args) =>
                 {
                     var data = args.Request.Data;
@@ -78,13 +92,26 @@ namespace wallabag.Data.ViewModels
                 };
                 DataTransferManager.ShowShareUI();
             });
-            EditTagsCommand = new RelayCommand(async () => await Services.DialogService.ShowAsync(Services.DialogService.Dialog.EditTags, new EditTagsViewModel(this.Model)));
-            OpenInBrowserCommand = new RelayCommand(async () => await Launcher.LaunchUriAsync(new Uri(Model.Url)));
+            EditTagsCommand = new RelayCommand(async () =>
+            {
+                LoggingService.WriteLine($"Editing tags of item {Model.Id}.");
+                await DialogService.ShowAsync(Dialogs.EditTagsDialog, new EditTagsViewModel(this.Model));
+            });
+            OpenInBrowserCommand = new RelayCommand(async () =>
+            {
+                LoggingService.WriteLine($"Opening item {Model.Id} in browser.");
+                await Launcher.LaunchUriAsync(new Uri(Model.Url));
+            });
         }
 
         public static ItemViewModel FromId(int itemId)
         {
+            var ls = SimpleIoc.Default.GetInstance<ILoggingService>();
+            ls.WriteLine($"Creating ItemViewModel from item id: {itemId}");
+
             var item = Database.Find<Item>(itemId);
+            ls.WriteLineIf(item == null, $"Failed! Item does not exist in database!", LoggingCategory.Critical);
+
             if (item != null)
                 return new ItemViewModel(item);
             else
@@ -93,6 +120,7 @@ namespace wallabag.Data.ViewModels
 
         private void UpdateItem()
         {
+            LoggingService.WriteLine($"Updating item {Model.Id} in database.");
             Model.LastModificationDate = DateTime.UtcNow;
             Database.Update(Model);
         }
