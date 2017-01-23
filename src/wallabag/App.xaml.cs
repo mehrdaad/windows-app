58a6a86488ba5dcc8680311ce2505a3fcfa48816
Julian Oster <jlnostr@outlook.de>
2017-01-23 16:43:54 +0100

    Move CustomBlobSerializer to library.

--- src/wallabag/App.xaml.cs
+++ src/wallabag/App.xaml.cs
@@ -183,32 +183,7 @@ namespace wallabag
         }
     }
 
-    public class CustomBlobSerializer : IBlobSerializer
-    {
-        private JsonSerializerSettings _serializerSettings = new JsonSerializerSettings()
-        {
-            TypeNameHandling = TypeNameHandling.All,
-            TypeNameAssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Full
-        };
 
-        public bool CanDeserialize(Type type) => true;
-
-        public object Deserialize(byte[] data, Type type)
-        {
-            string str = System.Text.Encoding.UTF8.GetString(data);
-
-            if (type == typeof(Uri))
-                return new Uri(str.Replace("\"", string.Empty));
-            else
-                return JsonConvert.DeserializeObject(str, _serializerSettings);
-        }
-
-        public byte[] Serialize<T>(T obj)
-        {
-            if (typeof(T) == typeof(Uri))
-                return System.Text.Encoding.UTF8.GetBytes(obj.ToString());
-            else
-                return System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj, _serializerSettings));
         }
     }
 }
