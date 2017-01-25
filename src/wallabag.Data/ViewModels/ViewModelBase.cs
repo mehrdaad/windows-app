c30a55e68a7ddabf19def187e9ebd039ac080d95
Julian Oster <jlnostr@outlook.de>
2017-01-25 20:27:36 +0100

    Set shared resources as read-only property on ViewModelBase.

--- src/wallabag.Data/ViewModels/ViewModelBase.cs
+++ src/wallabag.Data/ViewModels/ViewModelBase.cs
@@ -1,3 +1,5 @@
+using GalaSoft.MvvmLight.Ioc;
+using SQLite.Net;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using wallabag.Api;
@@ -8,11 +10,11 @@ namespace wallabag.Data.ViewModels
 {
     public class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase, INavigable
     {
-        internal IWallabagClient Client;
-        internal static SQLite.Net.SQLiteConnection Database;
-        internal INavigationService Navigation;
-        internal IDialogService DialogService;
-        internal ILoggingService LoggingService;
+        internal IWallabagClient Client => SimpleIoc.Default.GetInstance<IWallabagClient>();
+        internal static SQLiteConnection Database => SimpleIoc.Default.GetInstance<SQLiteConnection>();
+        internal INavigationService Navigation => SimpleIoc.Default.GetInstance<INavigationService>();
+        internal IDialogService DialogService => SimpleIoc.Default.GetInstance<IDialogService>();
+        internal ILoggingService LoggingService => SimpleIoc.Default.GetInstance<ILoggingService>();
 
         public virtual Task OnNavigatedToAsync(object parameter, IDictionary<string, object> state)
         {
