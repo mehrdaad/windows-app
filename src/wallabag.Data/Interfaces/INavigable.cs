ae080d277b12275111bc11d1323e0c81e98a4923
Julian Oster <jlnostr@outlook.de>
2017-01-31 22:06:38 +0100

    Execute INavigable implementation in NavigationService.

--- src/wallabag.Data/Interfaces/INavigable.cs
+++ src/wallabag.Data/Interfaces/INavigable.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace wallabag.Data.Common
 {
-    interface INavigable
+    public interface INavigable
     {
         Task OnNavigatedToAsync(object parameter, IDictionary<string, object> state);
         Task OnNavigatedFromAsync(IDictionary<string, object> pageState);
--- src/wallabag.UWP/Services/NavigationService.cs
+++ src/wallabag.UWP/Services/NavigationService.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Template10.Common;
+using wallabag.Data.Common;
 using wallabag.Data.Services;
+using Windows.UI.Xaml.Controls;
 using static wallabag.Data.Common.Navigation;
 
 namespace wallabag.Services
 {
     class NavigationService : INavigationService
     {
+        private ILoggingService _loggingService => SimpleIoc.Default.GetInstance<ILoggingService>();
+
         public string CurrentPageKey
         {
             get
@@ -30,18 +37,47 @@ namespace wallabag.Services
         }
 
         public void GoBack() => BootStrapper.Current.NavigationService.GoBack();
-        public void Navigate(Pages pageKey) => HandleNavigation(pageKey);
-        public void Navigate(Pages pageKey, object parameter) => HandleNavigation(pageKey, parameter);
+        public void Navigate(Pages pageKey) => HandleNavigationAsync(pageKey).ConfigureAwait(true);
+        public void Navigate(Pages pageKey, object parameter) => HandleNavigationAsync(pageKey, parameter).ConfigureAwait(true);
 
-        // TODO: Remove this piece of shit! NO ASYNC VOIDS! NEVER!
-        private async void HandleNavigation(Pages pageKey, object parameter = null)
+        private async Task HandleNavigationAsync(Pages pageKey, object parameter = null)
         {
             if (pageKey == Pages.AddItemPage)
                 await new Dialogs.AddItemDialog().ShowAsync();
             else if (pageKey == Pages.EditTagsPage)
                 await new Dialogs.EditTagsDialog().ShowAsync();
             else
-                BootStrapper.Current.NavigationService.Navigate(pageKey, parameter);
+            {
+                var ns = BootStrapper.Current.NavigationService;
+                _loggingService.WriteLine($"Navigating to {pageKey}. Type of parameter: {parameter?.GetType()?.Name}");
+
+                var newPageType = BootStrapper.Current.PageKeys<Pages>().First(p => p.Key == pageKey).Value;
+                var pageState = new Dictionary<string, object>();
+
+                var oldPage = ns.FrameFacade.Content as Page;
+                var oldViewModel = oldPage?.DataContext as INavigable;
+
+                _loggingService.WriteLine("Starting navigation...");
+                ns.FrameFacade.Navigate(newPageType, parameter, null);
+
+                if (oldViewModel != null)
+                {
+                    _loggingService.WriteLine($"Executing {nameof(INavigable.OnNavigatedFromAsync)} from old ViewModel ({oldViewModel?.GetType()?.Name ?? "NULL"}).");
+                    await oldViewModel.OnNavigatedFromAsync(pageState);
+                }
+                else _loggingService.WriteLine($"{nameof(INavigable.OnNavigatedFromAsync)} wasn't executed because the ViewModel was null.");
+
+                // fetch (current which is now new)
+                var newPage = ns.FrameFacade.Content as Page;
+                var newViewModel = newPage?.DataContext as INavigable;
+
+                if (newViewModel != null)
+                {
+                    _loggingService.WriteLine($"Executing {nameof(INavigable.OnNavigatedToAsync)} from new ViewModel ({newViewModel?.GetType()?.Name ?? "NULL"}).");
+                    await newViewModel.OnNavigatedToAsync(parameter, pageState);
+                }
+                else _loggingService.WriteLine($"{nameof(INavigable.OnNavigatedToAsync)} wasn't executed because the ViewModel was null.");
+            }
         }
 
         public void Configure(Pages pageKey, Type pageType)
