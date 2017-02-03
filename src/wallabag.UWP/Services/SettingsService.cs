e2df7684d4347fb6f5721fc89209b7475c4cc2ef
Julian Oster <jlnostr@outlook.de>
2017-02-03 20:53:09 +0100

    Get rid of T10 on SettingsService.

--- src/wallabag.UWP/Services/SettingsService.cs
+++ src/wallabag.UWP/Services/SettingsService.cs
@@ -1,55 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using wallabag.Data.Services;
+﻿using wallabag.Data.Services;
 using Windows.Storage;
 
 namespace wallabag.Services
 {
-    // TODO: Add serialization for non supported value types
-    class SettingsService : Data.Services.ISettingsService
+    class SettingsService : ISettingsService
     {
-        private List<Type> _supportedTypes = new List<Type>()
+        public T GetValueOrDefault<T>(string key, T defaultValue = default(T), SettingStrategy strategy = SettingStrategy.Local, string containerName = "")
         {
-            typeof(bool),
-            typeof(UInt16),
-            typeof(Int16),
-            typeof(Int32),
-            typeof(UInt32),
-            typeof(UInt64),
-            typeof(Single),
-            typeof(double),
-            typeof(char),
-            typeof(string),
-            typeof(DateTime),
-            typeof(TimeSpan),
-            typeof(Guid),
-            typeof(ApplicationDataCompositeValue)
-        };
-        private ApplicationDataContainer _localContainer => ApplicationData.Current.LocalSettings;
+            var container = GetContainerForStrategyAndName(strategy, containerName);
+            if (container.Values.ContainsKey(key))
+                return (T)container.Values[key];
 
-        public T GetValueOrDefault<T>(string key, T defaultValue = default(T), SettingStrategy strategy = SettingStrategy.Local, string container = "")
-        {
-            throw new NotImplementedException();
+            return defaultValue;
         }
 
-        public bool AddOrUpdateValue<T>(string key, T value, SettingStrategy strategy = SettingStrategy.Local, string container = "")
+        public void AddOrUpdateValue<T>(string key, T value, SettingStrategy strategy = SettingStrategy.Local, string containerName = "")
         {
-            throw new NotImplementedException();
+            var container = GetContainerForStrategyAndName(strategy, containerName);
+
+            container.Values[key] = value;
         }
 
-        public void Remove(string key, SettingStrategy strategy = SettingStrategy.Roaming, string container = "")
-        {
-            throw new NotImplementedException();
-        }
+        public void Remove(string key, SettingStrategy strategy = SettingStrategy.Roaming, string containerName = "")
+            => GetContainerForStrategyAndName(strategy, containerName).Values.Remove(key);
 
-        public void Clear(SettingStrategy strategy = SettingStrategy.Roaming, string container = "")
-        {
-            throw new NotImplementedException();
-        }
+        public void Clear(SettingStrategy strategy = SettingStrategy.Roaming, string containerName = "")
+            => GetContainerForStrategyAndName(strategy, containerName).Values.Clear();
 
-        public bool Contains(string key, SettingStrategy strategy = SettingStrategy.Local, string container = "")
+        public bool Contains(string key, SettingStrategy strategy = SettingStrategy.Local, string containerName = "")
+            => GetContainerForStrategyAndName(strategy, containerName).Values.ContainsKey(key);
+
+        private ApplicationDataContainer GetContainerForStrategyAndName(SettingStrategy strategy, string containerName)
         {
-            throw new NotImplementedException();
+            var container = ApplicationData.Current.LocalSettings;
+
+            if (strategy == SettingStrategy.Roaming)
+                container = ApplicationData.Current.RoamingSettings;
+
+            if (!string.IsNullOrWhiteSpace(containerName))
+                container = container.CreateContainer(containerName, ApplicationDataCreateDisposition.Always);
+
+            return container;
         }
     }
 }
