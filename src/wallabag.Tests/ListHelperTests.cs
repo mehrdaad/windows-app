be2d61dc60e2056f9a94527fcdcb96a6aff2aa55
Julian Oster <jlnostr@outlook.de>
2017-02-11 22:36:58 +0100

    Add ListHelperTests.

---
+++ src/wallabag.Tests/ListHelperTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace wallabag.Tests
+{
+    public class ListHelperTests
+    {
+        [Fact]
+        public void AscendingOrderSortsItemsAscending()
+        {
+            var items = CreateTestList();
+
+            string testItem = "Item 3.5";
+            Data.Common.Helpers.ListHelper.AddSorted(items, testItem, sortAscending: true);
+
+            Assert.True(items.Count == 10);
+            Assert.True(items.Contains(testItem));
+            Assert.True(items[4] == testItem);
+        }
+
+        [Fact]
+        public void DescendingOrderSortsItemsDescending()
+        {
+            var items = CreateTestList();
+
+            string testItem = "Item 3.5";
+            Data.Common.Helpers.ListHelper.AddSorted(items, testItem, sortAscending: false);
+
+            Assert.True(items.Count == 10);
+            Assert.True(items.Contains(testItem));
+            Assert.True(items[5] == testItem);
+        }
+
+        [Fact]
+        public void ReplacingACollectionReplacesAllItems()
+        {
+            var items = CreateTestList();
+            var newItems = new List<string>();
+
+            for (int i = 0; i < 3; i++)
+                newItems.Add($"New {i}");
+            
+            Data.Common.Helpers.ListHelper.Replace(items, newItems);
+
+            Assert.True(items.Count == newItems.Count);
+            Assert.All(items, item =>
+            {
+                Assert.Matches("New [0-9]", item);
+            });
+        }
+
+        private List<string> CreateTestList()
+        {
+            var items = new List<string>();
+
+            for (int i = 0; i < 9; i++)
+                items.Add($"Item {i}");
+
+            return items;
+        }
+    }
+}
