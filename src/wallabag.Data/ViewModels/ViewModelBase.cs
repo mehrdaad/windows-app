3aec7870e34685e3fcf578340ad8b7edf874fa8d
Julian Oster <jlnostr@outlook.de>
2017-02-13 21:42:24 +0100

    Remove interface properties from ViewModelBase.
    Constructor injection is the way to go!

--- src/wallabag.Data/ViewModels/ViewModelBase.cs
+++ src/wallabag.Data/ViewModels/ViewModelBase.cs
@@ -1,24 +1,11 @@
-using GalaSoft.MvvmLight.Ioc;
-using SQLite.Net;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using wallabag.Api;
 using wallabag.Data.Common;
-using wallabag.Data.Interfaces;
-using wallabag.Data.Services;
 
 namespace wallabag.Data.ViewModels
 {
     public class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase, INavigable
     {
-        internal IWallabagClient _client => SimpleIoc.Default.GetInstance<IWallabagClient>();
-        internal static SQLiteConnection _database => SimpleIoc.Default.GetInstance<SQLiteConnection>();
-        internal INavigationService _navigationService => SimpleIoc.Default.GetInstance<INavigationService>();
-        internal ILoggingService _loggingService => SimpleIoc.Default.GetInstance<ILoggingService>();
-
-        internal IDictionary<string, object> SessionState => SimpleIoc.Default.GetInstance<Dictionary<string, object>>("SessionState");
-        internal IPlatformSpecific Device => SimpleIoc.Default.GetInstance<IPlatformSpecific>();
-
         public virtual Task OnNavigatedToAsync(object parameter, IDictionary<string, object> state)
         {
             return Task.FromResult(true);
