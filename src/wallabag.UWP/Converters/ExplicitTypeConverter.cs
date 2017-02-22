55c9d51ce934d56eac229ee574273e651f67a280
Julian Oster <jlnostr@outlook.de>
2017-02-22 20:11:15 +0100

    Use an ExplicitTypeConverter for XAML bindings.
    Fortunately, I can remove this converter when I drop support for Windows TH2 (10586).

---
+++ src/wallabag.UWP/Converters/ExplicitTypeConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace wallabag.Converters
+{
+    public class ExplicitTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+            => System.Convert.ChangeType(value, targetType);
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => System.Convert.ChangeType(value, targetType);
+    }
+}
