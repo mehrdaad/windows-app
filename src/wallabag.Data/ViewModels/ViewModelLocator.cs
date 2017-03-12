f8d1e2f4b5c09c96bd31484f2bbbbb5709f72b73
Julian Oster <jlnostr@outlook.de>
2017-03-12 20:23:44 +0100

    Prevent duplicate registrations on ViewModelLocator.

--- src/wallabag.Data/ViewModels/ViewModelLocator.cs
+++ src/wallabag.Data/ViewModels/ViewModelLocator.cs
@@ -18,54 +18,57 @@ namespace wallabag.Data.ViewModels
             if (GalaSoft.MvvmLight.ViewModelBase.IsInDesignModeStatic)
                 return;
 
-            SimpleIoc.Default.Register<IWallabagClient>(() =>
+            if (!SimpleIoc.Default.IsRegistered<IWallabagClient>())
             {
-                var client = new WallabagClient(Settings.Authentication.WallabagUri, Settings.Authentication.ClientId, Settings.Authentication.ClientSecret);
-
-                if (!string.IsNullOrEmpty(Settings.Authentication.AccessToken) &&
-                    !string.IsNullOrEmpty(Settings.Authentication.RefreshToken))
+                SimpleIoc.Default.Register<IWallabagClient>(() =>
                 {
-                    client.AccessToken = Settings.Authentication.AccessToken;
-                    client.RefreshToken = Settings.Authentication.RefreshToken;
-                    client.LastTokenRefreshDateTime = Settings.Authentication.LastTokenRefreshDateTime;
-                }
+                    var client = new WallabagClient(Settings.Authentication.WallabagUri, Settings.Authentication.ClientId, Settings.Authentication.ClientSecret);
 
-                client.CredentialsRefreshed += (s, e) =>
+                    if (!string.IsNullOrEmpty(Settings.Authentication.AccessToken) &&
+                        !string.IsNullOrEmpty(Settings.Authentication.RefreshToken))
+                    {
+                        client.AccessToken = Settings.Authentication.AccessToken;
+                        client.RefreshToken = Settings.Authentication.RefreshToken;
+                        client.LastTokenRefreshDateTime = Settings.Authentication.LastTokenRefreshDateTime;
+                    }
+
+                    client.CredentialsRefreshed += (s, e) =>
+                    {
+                        Settings.Authentication.ClientId = client.ClientId;
+                        Settings.Authentication.ClientSecret = client.ClientSecret;
+                        Settings.Authentication.AccessToken = client.AccessToken;
+                        Settings.Authentication.RefreshToken = client.RefreshToken;
+                        Settings.Authentication.LastTokenRefreshDateTime = client.LastTokenRefreshDateTime;
+                    };
+
+                    return client;
+                });
+                SimpleIoc.Default.Register<SQLiteConnection>(() =>
                 {
-                    Settings.Authentication.ClientId = client.ClientId;
-                    Settings.Authentication.ClientSecret = client.ClientSecret;
-                    Settings.Authentication.AccessToken = client.AccessToken;
-                    Settings.Authentication.RefreshToken = client.RefreshToken;
-                    Settings.Authentication.LastTokenRefreshDateTime = client.LastTokenRefreshDateTime;
-                };
+                    var device = SimpleIoc.Default.GetInstance<Interfaces.IPlatformSpecific>();
 
-                return client;
-            });
-            SimpleIoc.Default.Register<SQLiteConnection>(() =>
-            {
-                var device = SimpleIoc.Default.GetInstance<Interfaces.IPlatformSpecific>();
+                    var db = new SQLiteConnection(device.GetSQLitePlatform(), device.GetDatabasePath(), serializer: new CustomBlobSerializer());
+                    db.CreateTable<Item>();
+                    db.CreateTable<Tag>();
+                    db.CreateTable<OfflineTask>();
 
-                var db = new SQLiteConnection(device.GetSQLitePlatform(), device.GetDatabasePath(), serializer: new CustomBlobSerializer());
-                db.CreateTable<Item>();
-                db.CreateTable<Tag>();
-                db.CreateTable<OfflineTask>();
+                    return db;
+                });
+                SimpleIoc.Default.Register(() => new Dictionary<string, object>(), "SessionState");
 
-                return db;
-            });
-            SimpleIoc.Default.Register(() => new Dictionary<string, object>(), "SessionState");
+                // TODO: Re-enable the generic ApiClientCreationService if cookie issue is done.
+                //SimpleIoc.Default.Register<IApiClientCreationService, ApiClientCreationService>();
+                SimpleIoc.Default.Register<IOfflineTaskService, OfflineTaskService>();
 
-            // TODO: Re-enable the generic ApiClientCreationService if cookie issue is done.
-            //SimpleIoc.Default.Register<IApiClientCreationService, ApiClientCreationService>();
-            SimpleIoc.Default.Register<IOfflineTaskService, OfflineTaskService>();
-
-            SimpleIoc.Default.Register<AddItemViewModel>();
-            SimpleIoc.Default.Register<EditTagsViewModel>();
-            SimpleIoc.Default.Register<ItemPageViewModel>();
-            SimpleIoc.Default.Register<LoginPageViewModel>();
-            SimpleIoc.Default.Register<MainViewModel>();
-            SimpleIoc.Default.Register<SettingsPageViewModel>();
-            SimpleIoc.Default.Register<MultipleSelectionViewModel>();
-            SimpleIoc.Default.Register<QRScanPageViewModel>();
+                SimpleIoc.Default.Register<AddItemViewModel>();
+                SimpleIoc.Default.Register<EditTagsViewModel>();
+                SimpleIoc.Default.Register<ItemPageViewModel>();
+                SimpleIoc.Default.Register<LoginPageViewModel>();
+                SimpleIoc.Default.Register<MainViewModel>();
+                SimpleIoc.Default.Register<SettingsPageViewModel>();
+                SimpleIoc.Default.Register<MultipleSelectionViewModel>();
+                SimpleIoc.Default.Register<QRScanPageViewModel>();
+            }
         }
 
         public AddItemViewModel AddItem => SimpleIoc.Default.GetInstanceWithoutCaching<AddItemViewModel>();
