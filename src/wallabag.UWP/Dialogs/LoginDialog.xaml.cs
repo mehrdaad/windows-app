804c164557de718d884d4b668496712a7237b1da
Julian Oster <jlnostr@outlook.de>
2017-03-29 21:44:37 +0200

    Finalize LoginDialog and remove redundant code.

--- src/wallabag.UWP/Dialogs/LoginDialog.xaml.cs
+++ src/wallabag.UWP/Dialogs/LoginDialog.xaml.cs
@@ -1,41 +1,27 @@
-﻿using GalaSoft.MvvmLight.Ioc;
-using wallabag.Api;
-using wallabag.Data.Common;
-using wallabag.Data.Services;
-using wallabag.Data.ViewModels;
+﻿using wallabag.ViewModels;
 using Windows.UI.Xaml.Controls;
 
 // Die Elementvorlage "Inhaltsdialogfeld" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
 
 namespace wallabag.Dialogs
 {
+    [PropertyChanged.ImplementPropertyChanged]
     public sealed partial class LoginDialog : ContentDialog
     {
-        public LoginPageViewModel ViewModel => DataContext as LoginPageViewModel;
+        private bool _blockClosing = true;
+        public LoginDialogViewModel ViewModel { get; private set; }
 
-        public LoginDialog() => InitializeComponent();
-
-        private async void ContentDialog_PrimaryButtonClickAsync(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        public LoginDialog()
         {
-            var logging = SimpleIoc.Default.GetInstance<ILoggingService>();
-            logging.WriteLine("Trying a re-login with given parameters.");
+            InitializeComponent();
+            ViewModel = new LoginDialogViewModel();
 
-            ViewModel.ClientId = Settings.Authentication.ClientId;
-            ViewModel.ClientSecret = Settings.Authentication.ClientSecret;
-            ViewModel.Url = Settings.Authentication.WallabagUri.ToString();
+            this.Closing += (s, e) => e.Cancel = _blockClosing;
 
-            bool result = await ViewModel.TestConfigurationAsync();
-            if (result)
-            {
-                logging.WriteLine("Re-login successful. Updating settings.");
-                var client = SimpleIoc.Default.GetInstance<IWallabagClient>();
-
-                Settings.Authentication.AccessToken = client.AccessToken;
-                Settings.Authentication.RefreshToken = client.RefreshToken;
-                Settings.Authentication.LastTokenRefreshDateTime = client.LastTokenRefreshDateTime;
-            }
-            else
-                logging.WriteLine("Re-login failed.");
+            ViewModel.ReloginCompleted += (s, e) => {
+                _blockClosing = false;
+                Hide();
+            };
         }
     }
 }
