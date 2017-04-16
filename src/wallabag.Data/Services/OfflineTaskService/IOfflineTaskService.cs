8940b39af7c424e4749b599db8b91c0de5ea7c33
Julian Oster <jlnostr@outlook.de>
2017-04-16 16:51:51 +0200

    Update IOfflineTaskService.
    Instead of subscribing to the CollectionChanged event, there are two new events which will make the UI handling far easier and the code more understandable.

--- src/wallabag.Data/Services/OfflineTaskService/IOfflineTaskService.cs
+++ src/wallabag.Data/Services/OfflineTaskService/IOfflineTaskService.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using wallabag.Data.Models;
 
@@ -7,11 +7,13 @@ namespace wallabag.Data.Services.OfflineTaskService
 {
     public interface IOfflineTaskService
     {
-        ObservableCollection<OfflineTask> Tasks { get; }
-        int LastItemId { get; }
+        int Count { get; }
 
         Task ExecuteAllAsync();
         void Add(string url, IEnumerable<string> newTags);
-        void Add(int itemId, OfflineTask.OfflineTaskAction action, List<Tag> addTagsList = null, List<Tag> removeTagsList = null);
+        void Add(int itemId, OfflineTask.OfflineTaskAction action, List<Tag> addedTags = null, List<Tag> removedTags = null);
+
+        event EventHandler<OfflineTask> TaskAdded;
+        event EventHandler<OfflineTaskExecutedEventArgs> TaskExecuted;
     }
 }
