a0a0bc8274dc922859390383d2ddf2169c78f086
Julian Oster <jlnostr@outlook.de>
2017-05-08 20:33:18 +0200

    Add constructors to the ChangelogEntry.

--- src/wallabag.Data/Models/ChangelogEntry.cs
+++ src/wallabag.Data/Models/ChangelogEntry.cs
@@ -5,6 +5,17 @@
         public string Label { get; set; }
         public ChangelogType Type { get; set; }
 
+        public ChangelogEntry()
+        {
+            Label = string.Empty;
+            Type = ChangelogType.Fix;
+        }
+        public ChangelogEntry(ChangelogType type, string label)
+        {
+            Label = label;
+            Type = type;
+        }
+
         public enum ChangelogType
         {
             Fix,
