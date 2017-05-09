ead092c5bffc7926c6e00ce83e9fba255a4efce1
Julian Oster <jlnostr@outlook.de>
2017-05-09 15:14:22 +0200

    Download NuGet during CI build.

---
+++ appveyor/build/RestoreNugetPackages.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Downloading NuGet..."
+$webclient = New-Object System.Net.WebClient
+$url = "https://dist.nuget.org/win-x86-commandline/v4.1.0/nuget.exe"
+$path = "$ENV:APPVEYOR_BUILD_FOLDER\nuget.exe"
+$webclient.DownloadFile($url, $path)
+
+Write-Host "Restoring NuGet packages..."
+& $path restore "$ENV:APPVEYOR_BUILD_FOLDER\wallabag.sln"
