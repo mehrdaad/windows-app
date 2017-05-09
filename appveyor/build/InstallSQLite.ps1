9fa22292168203267d811905f6a4bcdb39d7d380
Julian Oster <jlnostr@outlook.de>
2017-05-09 14:56:59 +0200

    Update CI scripts.

--- appveyor/build/InstallSQLite.ps1
+++ appveyor/build/InstallSQLite.ps1
@@ -3,6 +3,3 @@ $vsixPath = "$($env:USERPROFILE)\sqlite-uwp.vsix"
 "`"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
 & .\install-vsix.cmd
 Write-Host "Installed SQLite." -ForegroundColor green
-
-nuget restore
-Write-Host "Installed NuGet dependencies." -ForegroundColor green
