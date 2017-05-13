8b441d6c8eb5edd03c2076015392eff06add050b
Julian Oster <jlnostr@outlook.de>
2017-05-13 22:23:56 +0200

    Update SQLite to 3.18.

--- appveyor/build/InstallSQLite.ps1
+++ appveyor/build/InstallSQLite.ps1
@@ -1,5 +1,5 @@
 $vsixPath = "$($env:USERPROFILE)\sqlite-uwp.vsix"
-(New-Object Net.WebClient).DownloadFile('http://sqlite.org/2017/sqlite-uwp-3170000.vsix', $vsixPath)
+(New-Object Net.WebClient).DownloadFile('http://sqlite.org/2017/sqlite-uwp-3180000.vsix', $vsixPath)
 "`"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
 & .\install-vsix.cmd
 Write-Host "Installed SQLite." -ForegroundColor green
