d1a2d3a1c08c3da71ea547d1c395e6fe6c95a884
Julian Oster <jlnostr@outlook.de>
2017-06-01 16:05:19 +0200

    Restucture the LiveTileService for maintainability
    The look of the tile now depends on the size instead of using the same style for each tile. Also the readability of the code was improved.

--- src/wallabag.UWP/Services/LiveTileService.cs
+++ src/wallabag.UWP/Services/LiveTileService.cs
@@ -29,7 +29,7 @@ namespace wallabag.Services
             _logging.WriteLine("Updating live tile.");
 
             _logging.WriteLine("Fetching unread items from the database.");
-            var items = _database.Query<Item>("select Title,PreviewImageUri,EstimatedReadingTime,IsRead from Item WHERE PreviewImageUri IS NULL");
+            var items = _database.Query<Item>("select Title,PreviewImageUri,EstimatedReadingTime from Item where IsRead=0 ORDER BY CreationDate DESC");
             int maxPossibleTiles = Math.Min(5, items.Count);
 
             for (int i = 0; i < maxPossibleTiles; i++)
@@ -43,9 +43,9 @@ namespace wallabag.Services
                 {
                     Visual = new TileVisual()
                     {
-                        TileWide = CreateTileBinding(item.Title, item.EstimatedReadingTime, imageSource),
-                        TileMedium = CreateTileBinding(item.Title, item.EstimatedReadingTime, imageSource),
-                        TileLarge = CreateTileBinding(item.Title, item.EstimatedReadingTime, imageSource)
+                        TileWide = CreateTileBinding(TileSize.Wide, item.Title, item.EstimatedReadingTime, imageSource),
+                        TileMedium = CreateTileBinding(TileSize.Medium, item.Title, item.EstimatedReadingTime, imageSource),
+                        TileLarge = CreateTileBinding(TileSize.Large, item.Title, item.EstimatedReadingTime, imageSource)
                     }
                 };
 
@@ -59,43 +59,40 @@ namespace wallabag.Services
             }
         }
 
-        private TileBinding CreateTileBinding(string title, int readingTime, string imageSource)
+        private TileBinding CreateTileBinding(TileSize size, string title, int readingTime, string imageSource)
         {
             var binding = new TileBinding()
             {
                 Branding = TileBranding.Logo,
                 Content = new TileBindingContentAdaptive()
-                {
-                    Children =
-                    {
-                        new AdaptiveGroup()
-                        {
-                            Children =
-                            {
-                                new AdaptiveSubgroup()
-                                {
-                                    Children =
-                                    {
-                                        new AdaptiveText()
-                                        {
-                                            Text = title,
-                                            HintStyle = AdaptiveTextStyle.Caption,
-                                            HintWrap = true,
-                                            HintMaxLines=2
-                                        },
-                                        new AdaptiveText()
-                                        {
-                                            Text = $"Lesezeit: drei Minuten",
-                                            HintStyle = AdaptiveTextStyle.CaptionSubtle,
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
             };
 
+            // Create the XML in code
+            var group = new AdaptiveGroup();
+            var subgroup = new AdaptiveSubgroup();
+
+            // Add the title to the tile
+            var tileTitle = new AdaptiveText()
+            {
+                Text = title,
+                HintMaxLines = size == TileSize.Large ? 3 : 2,
+                HintWrap = true,
+                HintStyle = size == TileSize.Medium ? AdaptiveTextStyle.Caption : AdaptiveTextStyle.Body
+            };
+
+            subgroup.Children.Add(tileTitle);
+
+            // Add the reading time
+            subgroup.Children.Add(new AdaptiveText()
+            {
+                Text = $"Lesezeit: drei Minuten",
+                HintStyle = AdaptiveTextStyle.CaptionSubtle
+            });
+
+            // Finally add the subgroup to the tile
+            group.Children.Add(subgroup);
+
+            // Add the background image if neccessary
             if (!string.IsNullOrEmpty(imageSource))
             {
                 (binding.Content as TileBindingContentAdaptive).BackgroundImage = new TileBackgroundImage()
@@ -105,7 +102,12 @@ namespace wallabag.Services
                 };
             }
 
+            // Add the group to the tile
+            (binding.Content as TileBindingContentAdaptive).Children.Add(group);
+
             return binding;
         }
+
+        private enum TileSize { Small, Medium, Wide, Large }
     }
 }
