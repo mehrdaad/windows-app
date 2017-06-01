aa9ff9868320c80ed316c866739b92fa1b50ca2b
Julian Oster <jlnostr@outlook.de>
2017-06-01 17:24:13 +0200

    Update the live tile if the settings have changed.

--- src/wallabag.UWP/Common/Settings.cs
+++ src/wallabag.UWP/Common/Settings.cs
@@ -1,19 +1,32 @@
-﻿namespace wallabag.Common
+﻿using GalaSoft.MvvmLight.Ioc;
+using wallabag.Services;
+
+namespace wallabag.Common
 {
     public class Settings : Data.Common.Settings
     {
         public class LiveTile
         {
+            private static void UpdateLiveTile() => SimpleIoc.Default.GetInstance<LiveTileService>().Update();
+
             public static bool IsEnabled
             {
                 get => SettingsService.GetValueOrDefault(nameof(IsEnabled), true, containerName: nameof(LiveTile));
-                set => SettingsService.AddOrUpdateValue(nameof(IsEnabled), value, containerName: nameof(LiveTile));
+                set
+                {
+                    SettingsService.AddOrUpdateValue(nameof(IsEnabled), value, containerName: nameof(LiveTile));
+                    UpdateLiveTile();
+                }
             }
 
             public static ItemType DisplayedItemType
             {
                 get => SettingsService.GetValueOrDefault(nameof(DisplayedItemType), ItemType.Unread, containerName: nameof(LiveTile));
-                set => SettingsService.AddOrUpdateValue(nameof(DisplayedItemType), value, containerName: nameof(LiveTile));
+                set
+                {
+                    SettingsService.AddOrUpdateValue(nameof(DisplayedItemType), value, containerName: nameof(LiveTile));
+                    UpdateLiveTile();
+                }
             }
 
             public enum ItemType { Unread, Starred, Archived }
