6dce7f8965c5f5be1429416c21a671c05f358388
Julian Oster <jlnostr@outlook.de>
2017-06-01 16:31:27 +0200

    Add settings for the live tile.

---
+++ src/wallabag.UWP/Common/Settings.cs
@@ -0,0 +1,22 @@
+﻿namespace wallabag.Common
+{
+    public class Settings : Data.Common.Settings
+    {
+        public class LiveTile
+        {
+            public static bool IsEnabled
+            {
+                get => SettingsService.GetValueOrDefault(nameof(IsEnabled), true, containerName: nameof(LiveTile));
+                set => SettingsService.AddOrUpdateValue(nameof(IsEnabled), value, containerName: nameof(LiveTile));
+            }
+
+            public static ItemType DisplayedItemType
+            {
+                get => SettingsService.GetValueOrDefault(nameof(DisplayedItemType), ItemType.Unread, containerName: nameof(LiveTile));
+                set => SettingsService.AddOrUpdateValue(nameof(DisplayedItemType), value, containerName: nameof(LiveTile));
+            }
+
+            public enum ItemType { Unread, Starred, Archived }
+        }
+    }
+}
