06be5097da3276985f42faf08ecc1855104026c6
Julian Oster <jlnostr@outlook.de>
2017-06-09 16:56:10 +0200

    Add draft for a StartPage including its ViewModel.
    The StartPage will display an indicator for migrations and a list of changes since the last release. After it's done, it will navigate to the next page, which in most cases is the MainPage.

---
+++ src/wallabag.UWP/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using wallabag.Data.ViewModels;
+
+namespace wallabag.ViewModels
+{
+    public class StartPageViewModel : ViewModelBase
+    {
+        public bool IsActive { get; set; }
+        public string ProgressDescription { get; set; }
+
+        public StartPageViewModel()
+        {
+
+        }
+    }
+}
---
+++ src/wallabag.UWP/Views/StartPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using wallabag.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace wallabag.Views
+{
+    public sealed partial class StartPage : Page
+    {
+        public StartPageViewModel ViewModel => (DataContext as StartPageViewModel);
+        public StartPage() => InitializeComponent();
+    }
+}
