66045da7cab21547a02d5895dc1702cdcad94075
Julian Oster <jlnostr@outlook.de>
2017-07-05 13:24:17 +0200

    Fix MigrationServiceTests.

--- src/wallabag.Tests/MigrationServiceTests.cs
+++ src/wallabag.Tests/MigrationServiceTests.cs
@@ -10,83 +10,6 @@ namespace wallabag.Tests
     public class MigrationServiceTests
     {
         [Fact]
-        public void AddingAFeatureThroughTheExtension()
-        {
-            var testMigration = Migration.Create("1.0.0").AddFeature("test");
-
-            Assert.Equal(1, testMigration.ChangelogEntries.Count);
-            Assert.Equal("test", testMigration.ChangelogEntries[0].Title);
-            Assert.Equal(testMigration.ChangelogEntries[0].Type, Data.Models.ChangelogEntry.ChangelogType.Feature);
-
-            testMigration.AddFeature("test 2", "my description");
-
-            Assert.Equal(2, testMigration.ChangelogEntries.Count);
-            Assert.Equal(testMigration.ChangelogEntries[1].Description, "my description");
-            Assert.Equal(testMigration.ChangelogEntries[1].Type, Data.Models.ChangelogEntry.ChangelogType.Feature);
-        }
-
-        [Fact]
-        public void AddingABugfixThroughTheExtension()
-        {
-            var testMigration = Migration.Create("1.0.0").AddBugfix("test");
-
-            Assert.Equal(1, testMigration.ChangelogEntries.Count);
-            Assert.Equal("test", testMigration.ChangelogEntries[0].Title);
-            Assert.Equal(testMigration.ChangelogEntries[0].Type, Data.Models.ChangelogEntry.ChangelogType.Fix);
-
-            testMigration.AddBugfix("test 2", "my description");
-
-            Assert.Equal(2, testMigration.ChangelogEntries.Count);
-            Assert.Equal("my description", testMigration.ChangelogEntries[1].Description);
-            Assert.Equal(testMigration.ChangelogEntries[1].Type, Data.Models.ChangelogEntry.ChangelogType.Fix);
-        }
-
-        [Fact]
-        public void AddingAnEnhancementThroughTheExtension()
-        {
-            var testMigration = Migration.Create("1.0.0").AddEnhancement("test");
-
-            Assert.Equal(1, testMigration.ChangelogEntries.Count);
-            Assert.Equal("test", testMigration.ChangelogEntries[0].Title);
-            Assert.Equal(testMigration.ChangelogEntries[0].Type, Data.Models.ChangelogEntry.ChangelogType.Enhancement);
-
-            testMigration.AddEnhancement("test 2", "my description");
-
-            Assert.Equal(2, testMigration.ChangelogEntries.Count);
-            Assert.Equal(testMigration.ChangelogEntries[1].Description, "my description");
-            Assert.Equal(testMigration.ChangelogEntries[1].Type, Data.Models.ChangelogEntry.ChangelogType.Enhancement);
-        }
-
-        [Fact]
-        public void CompletionThroughExtensionAddsMigrationToService()
-        {
-            var fakeMigrationService = A.Fake<IMigrationService>();
-
-            Migration.Create("1.0.0").AddEnhancement("test").Complete(fakeMigrationService);
-
-            A.CallTo(() => fakeMigrationService.Add(A<Migration>.Ignored)).MustHaveHappened();
-        }
-
-        [Fact]
-        public void ChangelogOnlyReturnsEntriesForNewerVersionsThanTheOldOne()
-        {
-            var logging = A.Fake<ILoggingService>();
-            var device = A.Fake<IPlatformSpecific>();
-            var migrationService = new MigrationService(logging, device);
-
-            A.CallTo(() => device.AppVersion).Returns("9.0.0");
-
-            for (int i = 1; i <= 10; i++)
-                Migration.Create($"{i}.0.0")
-                    .AddFeature($"Feature {i}")
-                    .Complete(migrationService);
-
-            var changelog = migrationService.GetChangelog(new Version("5.0.0"));
-
-            Assert.Equal(4, changelog.Count);
-        }
-
-        [Fact]
         public void ExecutionOnlyExecutesMigrationsNewerThanTheOldVersion()
         {
             var logging = A.Fake<ILoggingService>();
@@ -97,9 +20,7 @@ namespace wallabag.Tests
 
             int migrationCounter = 0;
             for (int i = 1; i <= 10; i++)
-                Migration.Create($"{i}.0.0")
-                    .SetMigrationAction(() => migrationCounter++)
-                    .Complete(migrationService);
+                migrationService.Create($"{i}.0.0", () => migrationCounter++);
 
             migrationService.ExecuteAll(new Version("5.0.0"));
             Assert.Equal(4, migrationCounter);
@@ -116,9 +37,7 @@ namespace wallabag.Tests
 
             int migrationCounter = 0;
             for (int i = 1; i <= 10; i++)
-                Migration.Create($"{i}.0.0")
-                    .SetMigrationAction(() => migrationCounter++)
-                    .Complete(migrationService);
+                migrationService.Create($"{i}.0.0", () => migrationCounter++);
 
             migrationService.ExecuteAll(new Version("9.0.0"));
             Assert.Equal(0, migrationCounter);
@@ -141,38 +60,12 @@ namespace wallabag.Tests
             A.CallTo(() => device.AppVersion).Returns("9.0.0");
 
             int currentMajorVersion = 0;
-            Migration.Create($"4.0.0")
-                .SetMigrationAction(() => currentMajorVersion = 4)
-                .Complete(migrationService);
-            Migration.Create($"2.0.0")
-                .SetMigrationAction(() => currentMajorVersion = 2)
-                .Complete(migrationService);
-            Migration.Create($"3.0.0")
-                .SetMigrationAction(() => currentMajorVersion = 3)
-                .Complete(migrationService);
+            migrationService.Create($"4.0.0", () => currentMajorVersion = 4);
+            migrationService.Create($"2.0.0", () => currentMajorVersion = 2);
+            migrationService.Create($"3.0.0", () => currentMajorVersion = 3);
 
             migrationService.ExecuteAll(new Version("1.0.0"));
             Assert.Equal(4, currentMajorVersion);
-        }
-
-        [Fact]
-        public void NotExistingChangelogForSpecificVersionReturnsEmptyList()
-        {
-            var logging = A.Fake<ILoggingService>();
-            var device = A.Fake<IPlatformSpecific>();
-            var migrationService = new MigrationService(logging, device);
-
-            A.CallTo(() => device.AppVersion).Returns("9.0.0");
-
-            for (int i = 1; i <= 6; i++)
-                Migration.Create($"{6 - i}.0.0")
-                    .AddBugfix("My bugfix")
-                    .Complete(migrationService);
-
-            var changelog = migrationService.GetChangelog(new Version("8.0.0"));
-
-            Assert.NotNull(changelog);
-            Assert.Equal(0, changelog.Count);
         }
     }
 }
